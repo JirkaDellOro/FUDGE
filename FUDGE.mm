b14a91c1ca05c69c5dd66447330c913b937d3729
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2018-03-02 10:18:28 +0100

    MindMap initiated

---
+++ FUDGE.mm
@@ -0,0 +1,110 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1283093380553" ID="ID_1723255651" MODIFIED="1519981806451" TEXT="FUDGE">
+<edge COLOR="#006666"/>
+<hook NAME="AutomaticEdgeColor"/>
+<hook NAME="MapStyle">
+<properties SHOW_NOTE_ICONS="true"/>
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="as_parent">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge/>
+<cloud/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font/>
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font/>
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font/>
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font/>
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node CREATED="1519980459055" FOLDED="true" ID="ID_1508151389" MODIFIED="1519981813828" POSITION="right" TEXT="General Info">
+<edge COLOR="#006666"/>
+<node CREATED="1519980472563" ID="ID_502278214" MODIFIED="1519981806451" TEXT="FUDGE means Furtwangen University Didactic Game Engine">
+<edge COLOR="#006666"/>
+</node>
+<node CREATED="1519980494735" ID="ID_1712624881" MODIFIED="1519981806451" TEXT="Idea is to create a simple game engine to be used for didactic purpose in the first place">
+<edge COLOR="#006666"/>
+</node>
+<node CREATED="1519980522805" ID="ID_284858189" MODIFIED="1519981806451" TEXT="Maybe to become a &quot;real&quot; game engine in the long run">
+<edge COLOR="#006666"/>
+</node>
+<node CREATED="1519980541868" ID="ID_1977209159" MODIFIED="1519981806451" TEXT="Inspired by Animate (Flash) and Unity">
+<edge COLOR="#006666"/>
+</node>
+<node CREATED="1519980572907" FOLDED="true" ID="ID_469784332" MODIFIED="1519981806451" TEXT="Games, Engine and IDE based on web-technologies">
+<edge COLOR="#006666"/>
+<node CREATED="1519980608983" ID="ID_317462148" MODIFIED="1519980610781" TEXT="HTML"/>
+<node CREATED="1519980942859" ID="ID_488564373" MODIFIED="1519980951074" TEXT="HTMLCanvas"/>
+<node CREATED="1519980611562" ID="ID_64249759" MODIFIED="1519980612625" TEXT="CSS"/>
+<node CREATED="1519980612828" ID="ID_463512426" MODIFIED="1519980623859" TEXT="TypeScript/Javascript"/>
+<node CREATED="1519980624384" ID="ID_1650271823" MODIFIED="1519980626278" TEXT="Node"/>
+<node CREATED="1519980926184" ID="ID_1938690081" MODIFIED="1519980928500" TEXT="WebGL"/>
+<node CREATED="1519980629205" FOLDED="true" ID="ID_47935556" MODIFIED="1519980641610" TEXT="Libraries">
+<node CREATED="1519980641625" FOLDED="true" ID="ID_1876489318" MODIFIED="1519980662910" TEXT="CreateJS">
+<node CREATED="1519980663182" ID="ID_1956633705" MODIFIED="1519980776902" TEXT="EaselJS"/>
+<node CREATED="1519980778185" ID="ID_369478536" MODIFIED="1519980785484" TEXT="TweenJS"/>
+<node CREATED="1519980786063" ID="ID_1703640757" MODIFIED="1519980790207" TEXT="SoundJS"/>
+<node CREATED="1519980792076" ID="ID_1243244143" MODIFIED="1519980797018" TEXT="PreloadJS"/>
+</node>
+<node CREATED="1519980802800" ID="ID_1357570091" MODIFIED="1519980806434" TEXT="ThreeJS"/>
+</node>
+</node>
+<node CREATED="1519981085922" FOLDED="true" ID="ID_1081458525" MODIFIED="1519981806451" TEXT="Execute in Browser or Standalone">
+<edge COLOR="#006666"/>
+<node CREATED="1519981104177" ID="ID_855287814" MODIFIED="1519981110810" TEXT="Electron"/>
+<node CREATED="1519981111239" ID="ID_1074381517" MODIFIED="1519981125190" TEXT="PhoneGap/Cordova"/>
+</node>
+<node CREATED="1519981233950" ID="ID_1245528493" MODIFIED="1519981806451" TEXT="IDE displays scenes and allows manipulation">
+<edge COLOR="#006666"/>
+</node>
+</node>
+<node CREATED="1519981138686" FOLDED="true" ID="ID_894632498" MODIFIED="1519981813079" POSITION="right" TEXT="Paradigms">
+<edge COLOR="#006666"/>
+<node CREATED="1519981143486" ID="ID_1612958640" MODIFIED="1519981806451" TEXT="Composition over Inheritance (COI)">
+<edge COLOR="#006666"/>
+</node>
+<node CREATED="1519981159851" ID="ID_1895498609" MODIFIED="1519981806451" TEXT="Modular System">
+<edge COLOR="#006666"/>
+</node>
+<node CREATED="1519981167500" FOLDED="true" ID="ID_417993702" MODIFIED="1519981806451" TEXT="Game-Scripting with TypeScript">
+<edge COLOR="#006666"/>
+<node CREATED="1519981197294" ID="ID_1919438545" MODIFIED="1519981208404" TEXT="Definition files must be created"/>
+</node>
+</node>
+</node>
+</map>
