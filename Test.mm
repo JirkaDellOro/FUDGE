f07597f3b2415ea6cbb7aab21b14077b4ebf8f6e
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2018-03-02 09:35:19 +0100

    Test docear mindmap

---
+++ Test.mm
@@ -0,0 +1,64 @@
+<map version="docear 1.1" project="161E5D10F229FT7DG1ALF0V2WDUFRJFV9YDB" project_last_home="file:/C:/Users/jirka/Docear/projects/Test" dcr_id="1519979323436_8wpd8pvpir8fmqgwllw5aovbv">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Test" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1519979332084"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="Ein erster Testknoten" POSITION="right" ID="ID_1326772704" CREATED="1519979333952" MODIFIED="1519979340703">
+<edge COLOR="#ff0000"/>
+<node TEXT="Kindknoten1" ID="ID_1150719544" CREATED="1519979344160" MODIFIED="1519979349193"/>
+<node TEXT="Kindknoten2" ID="ID_250693869" CREATED="1519979349812" MODIFIED="1519979353625"/>
+<node TEXT="Link zum ersten Testknoten" ID="ID_1294900521" CREATED="1519979354422" MODIFIED="1519979532994" LINK="#ID_1326772704"/>
+<node TEXT="externer Link" ID="ID_1754194828" CREATED="1519979363657" MODIFIED="1519979430139">
+<node TEXT="www.entertrain.com" ID="ID_1951432714" CREATED="1519979430155" MODIFIED="1519979458032" LINK="http://www.entertrain.com"/>
+</node>
+</node>
+</node>
+</map>
