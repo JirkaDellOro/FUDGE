3701c711741677be8ebf949022c55f994142f0a7
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2018-09-13 12:44:42 +0200

    Robels Thesis hochgeladen

---
+++ Quellen/ThesisRobel-ElectronAnimation/Quellcode/src/app/editor/editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css']
+})
+export class EditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Quellen/ThesisRobel-ElectronAnimation/readme.md
+++
@@ -1,1 +0,0 @@
-Robels Thesis
