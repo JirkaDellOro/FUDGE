014d8135af32749e419fc73c809e99882daa348c
Jirka <del@hs-furtwangen.de>
2018-11-11 10:26:27 +0100

    Test namespace and naming

---
+++ Experiments/Jirka/NameSpace/Fudge.ts
@@ -0,0 +1,10 @@
+/**
+ * Performance tests for various types of nodes that my build a 3D-scene
+ */
+namespace Fudge {
+    export class Test {
+        static sayHello():void {
+            console.log("Hello from the Test-Class of Fudge");
+        }
+    }
+}
---
+++ Experiments/Jirka/NameSpace/Main..ts
@@ -0,0 +1,8 @@
+/**
+ * Testing namespace/module structure and naming conventions
+ */
+namespace NameSpaceTest {
+    import ƒ = Fudge;
+
+    ƒ.Test.sayHello();
+}
