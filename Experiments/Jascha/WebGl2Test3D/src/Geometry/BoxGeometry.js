a8e1d692fb89f299f410f2291ca2588ec21fd364
R4Pi3R <jascha.karagoel@hs-furtwangen.de>
2019-02-23 13:48:05 +0100

    removed unnecessary attributes

--- Experiments/Jascha/WebGl2Test3D/src/Geometry/BoxGeometry.js
+++ Experiments/Jascha/WebGl2Test3D/src/Geometry/BoxGeometry.js
@@ -5,53 +5,50 @@ var WebEngine;
      */
     class BoxGeometry {
         constructor(_width, _height, _depth) {
-            this.width = _width;
-            this.height = _height;
-            this.depth = _depth;
-            this.positions = [
+            this.positions = new Float32Array([
                 //front
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+                _width / 2, _height / 2, _depth / 2,
                 //back
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, -_depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
                 //left
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, this.depth / 2,
+                -_width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
                 //right
-                this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
                 //top
-                -this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
                 //bottom
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, -this.height / 2, this.depth / 2,
-            ];
+                -_width / 2, -_height / 2, -_depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+            ]);
         }
         // Get method.######################################################################################
         get Positions() {
--- Experiments/Jascha/WebGl2Test3D/src/Geometry/BoxGeometry.ts
+++ Experiments/Jascha/WebGl2Test3D/src/Geometry/BoxGeometry.ts
@@ -4,67 +4,60 @@ namespace WebEngine {
      * Simple class to compute the vertexpositions for a box.
      */
     export class BoxGeometry {
-
-        private width: number; // The box' width.
-        private height: number; // The box' height.
-        private depth: number; // The box' depth.
-        private positions: number[]; //Array holding x, y, and z-positions for each vertex.
+        private positions : Float32Array
 
         public constructor(_width: number, _height: number, _depth: number) {
 
-            this.width = _width;
-            this.height = _height;
-            this.depth = _depth;
-            this.positions = [
+            this.positions = new Float32Array([
                 
                 //front
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+                _width / 2, _height / 2, _depth / 2,
 
                 //back
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, -_depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
 
                 //left
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, this.depth / 2,
+                -_width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
 
                 //right
 
-                this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
+                _width / 2, -_height / 2, _depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
 
                 //top
-                -this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                -this.width / 2, this.height / 2, -this.depth / 2,
-                this.width / 2, this.height / 2, this.depth / 2,
-                this.width / 2, this.height / 2, -this.depth / 2,
+                -_width / 2, _height / 2, _depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                -_width / 2, _height / 2, -_depth / 2,
+                _width / 2, _height / 2, _depth / 2,
+                _width / 2, _height / 2, -_depth / 2,
 
                 //bottom
-                -this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                -this.width / 2, -this.height / 2, this.depth / 2,
-                this.width / 2, -this.height / 2, -this.depth / 2,
-                this.width / 2, -this.height / 2, this.depth / 2,];
+                -_width / 2, -_height / 2, -_depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                -_width / 2, -_height / 2, _depth / 2,
+                _width / 2, -_height / 2, -_depth / 2,
+                _width / 2, -_height / 2, _depth / 2,]);
         }
 
         // Get method.######################################################################################
