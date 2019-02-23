4525db8b87eb3b2b72da3e53ccd4ddeb0c1f4b4b
R4Pi3R <jascha.karagoel@hs-furtwangen.de>
2019-02-23 13:45:08 +0100

    Changes in organisation of Shader-classes

--- Experiments/Jascha/WebGl2Test3D/src/Shader/BasicShader.js
+++ Experiments/Jascha/WebGl2Test3D/src/Shader/BasicShader.js
@@ -1,69 +1,36 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var WebEngine;
 (function (WebEngine) {
     /**
      * Represents a WebGL shaderprogram
      */
-    class BasicShader extends WebEngine.Shader {
-        constructor() {
-            super();
-            this.load(this.loadVertexShaderSource(), this.loadFragmentShaderSource());
+    var BasicShader = /** @class */ (function (_super) {
+        __extends(BasicShader, _super);
+        function BasicShader() {
+            var _this = _super.call(this) || this;
+            _this.load(_this.loadVertexShaderSource(), _this.loadFragmentShaderSource());
+            return _this;
         }
-        loadVertexShaderSource() {
-            return `#version 300 es
- 
-        // an attribute is an input (in) to a vertex shader.
-        // It will receive data from a buffer
-        in vec4 a_position;
-        in vec4 a_color;
-        in vec2 a_textureCoordinate;
-    
-        // The Matrix to transform the positions by.
-        uniform mat4 u_matrix;
-    
-    
-        // Varying color in the fragmentshader.
-        out vec4 v_color;
-        // Varying texture in the fragmentshader.
-        out vec2 v_textureCoordinate;
-    
-    
-        // all shaders have a main function.
-        void main() {  
-            // Multiply all positions by the matrix.   
-            vec4 position = u_matrix * a_position;
-    
-    
-            gl_Position = u_matrix * a_position;
-    
-            // Pass color to fragmentshader.
-            v_color = a_color;
-            v_textureCoordinate = a_textureCoordinate;
-        }
-        `;
-        }
-        loadFragmentShaderSource() {
-            return `#version 300 es
-     
-            // fragment shaders don't have a default precision so we need
-            // to pick one. mediump is a good default. It means "medium precision"
-            precision mediump float;
-            
-            // Color passed from vertexshader.
-            in vec4 v_color;
-            // Texture passed from vertexshader.
-            in vec2 v_textureCoordinate;
-        
-        
-            uniform sampler2D u_texture;
-            // we need to declare an output for the fragment shader
-            out vec4 outColor;
-            
-            void main() {
-            outColor = v_color;
-            outColor = texture(u_texture, v_textureCoordinate) * v_color;
-            }`;
-        }
-    }
+        BasicShader.prototype.loadVertexShaderSource = function () {
+            return "#version 300 es\n \n        // an attribute is an input (in) to a vertex shader.\n        // It will receive data from a buffer\n        in vec4 a_position;\n        in vec4 a_color;\n        in vec2 a_textureCoordinate;\n    \n        // The Matrix to transform the positions by.\n        uniform mat4 u_matrix;\n    \n    \n        // Varying color in the fragmentshader.\n        out vec4 v_color;\n        // Varying texture in the fragmentshader.\n        out vec2 v_textureCoordinate;\n    \n    \n        // all shaders have a main function.\n        void main() {  \n            // Multiply all positions by the matrix.   \n            vec4 position = u_matrix * a_position;\n    \n    \n            gl_Position = u_matrix * a_position;\n    \n            // Pass color to fragmentshader.\n            v_color = a_color;\n            v_textureCoordinate = a_textureCoordinate;\n        }\n        ";
+        };
+        BasicShader.prototype.loadFragmentShaderSource = function () {
+            return "#version 300 es\n     \n            // fragment shaders don't have a default precision so we need\n            // to pick one. mediump is a good default. It means \"medium precision\"\n            precision mediump float;\n            \n            // Color passed from vertexshader.\n            in vec4 v_color;\n            // Texture passed from vertexshader.\n            in vec2 v_textureCoordinate;\n        \n        \n            uniform sampler2D u_texture;\n            // we need to declare an output for the fragment shader\n            out vec4 outColor;\n            \n            void main() {\n            outColor = v_color;\n            outColor = texture(u_texture, v_textureCoordinate) * v_color;\n            }";
+        };
+        return BasicShader;
+    }(WebEngine.Shader));
     WebEngine.BasicShader = BasicShader;
 })(WebEngine || (WebEngine = {}));
 //# sourceMappingURL=BasicShader.js.map
--- Experiments/Jascha/WebGl2Test3D/src/Shader/Shader.js
+++ Experiments/Jascha/WebGl2Test3D/src/Shader/Shader.js
@@ -3,11 +3,11 @@ var WebEngine;
     /**
      * Abstract superclass for the representation of WebGl shaderprograms.
      */
-    class Shader {
+    var Shader = /** @class */ (function () {
         /**
          * Creates a new shader.
          */
-        constructor() {
+        function Shader() {
             this.attributes = {}; // Associative array of shader atrributes.
             this.uniforms = {}; // Associative array of shader uniforms.
         }
@@ -16,40 +16,40 @@ var WebEngine;
          * Get location of an attribute by its name.
          * @param _name Name of the attribute to locate.
          */
-        getAttributeLocation(_name) {
+        Shader.prototype.getAttributeLocation = function (_name) {
             if (this.attributes[_name] === undefined) {
                 return null;
             }
             return this.attributes[_name];
-        }
+        };
         /**
           * Get location of uniform by its name.
           * @param _name Name of the attribute to locate.
           */
-        getUniformLocation(_name) {
+        Shader.prototype.getUniformLocation = function (_name) {
             if (this.uniforms[_name] === undefined) {
                 return null;
             }
             return this.uniforms[_name];
-        }
-        load(_vertexShaderSource, _fragmentShaderSource) {
-            let vertexShader = this.loadShader(_vertexShaderSource, WebEngine.gl2.VERTEX_SHADER);
-            let fragmentShader = this.loadShader(_fragmentShaderSource, WebEngine.gl2.FRAGMENT_SHADER);
+        };
+        Shader.prototype.load = function (_vertexShaderSource, _fragmentShaderSource) {
+            var vertexShader = this.loadShader(_vertexShaderSource, WebEngine.gl2.VERTEX_SHADER);
+            var fragmentShader = this.loadShader(_fragmentShaderSource, WebEngine.gl2.FRAGMENT_SHADER);
             this.createProgram(vertexShader, fragmentShader);
             this.detectAttributes();
             this.detectUniforms();
-        }
+        };
         // Utility methods.######################################################################################
         /**
          * Compiles shader from sourcestring.
          * @param _source The sourcevariable holding a GLSL shaderstring.
          * @param _shaderType The type of the shader to be compiled. (vertex or fragment).
          */
-        loadShader(_source, _shaderType) {
-            let shader = WebEngine.gl2.createShader(_shaderType);
+        Shader.prototype.loadShader = function (_source, _shaderType) {
+            var shader = WebEngine.gl2.createShader(_shaderType);
             WebEngine.gl2.shaderSource(shader, _source);
             WebEngine.gl2.compileShader(shader);
-            let error = WebEngine.gl2.getShaderInfoLog(shader);
+            var error = WebEngine.gl2.getShaderInfoLog(shader);
             if (error !== "") {
                 throw new Error("Error compiling shader: " + error);
             }
@@ -59,55 +59,56 @@ var WebEngine;
                 return null;
             }
             return shader;
-        }
+        };
         /**
          * Create shaderprogramm that will be used on GPU.
          * @param vertexShader The compiled vertexshader to be used by the programm.
          * @param fragmentShader The compiled fragmentshader to be used by the programm.
          */
-        createProgram(vertexShader, fragmentShader) {
+        Shader.prototype.createProgram = function (vertexShader, fragmentShader) {
             this.program = WebEngine.gl2.createProgram();
             WebEngine.gl2.attachShader(this.program, vertexShader);
             WebEngine.gl2.attachShader(this.program, fragmentShader);
             WebEngine.gl2.linkProgram(this.program);
-            let error = WebEngine.gl2.getProgramInfoLog(this.program);
+            var error = WebEngine.gl2.getProgramInfoLog(this.program);
             if (error !== "") {
                 throw new Error("Error linking Shader: " + error);
             }
-        }
+        };
         /**
          * Use this shader in Rendercontext on callup.
          */
-        use() {
+        Shader.prototype.use = function () {
             WebEngine.gl2.useProgram(this.program);
-        }
+        };
         /**
          * Iterates through all active attributes on an instance of shader and saves them in an associative array with the attribute's name as key and the location as value
          */
-        detectAttributes() {
-            let attributeCount = WebEngine.gl2.getProgramParameter(this.program, WebEngine.gl2.ACTIVE_ATTRIBUTES);
-            for (let i = 0; i < attributeCount; i++) {
-                let attributeInfo = WebEngine.gl2.getActiveAttrib(this.program, i);
+        Shader.prototype.detectAttributes = function () {
+            var attributeCount = WebEngine.gl2.getProgramParameter(this.program, WebEngine.gl2.ACTIVE_ATTRIBUTES);
+            for (var i = 0; i < attributeCount; i++) {
+                var attributeInfo = WebEngine.gl2.getActiveAttrib(this.program, i);
                 if (!attributeInfo) {
                     break;
                 }
                 this.attributes[attributeInfo.name] = WebEngine.gl2.getAttribLocation(this.program, attributeInfo.name);
             }
-        }
+        };
         /**
         * Iterates through all active uniforms on an instance of shader and saves them in an associative array with the attribute's name as key and the location as value
         */
-        detectUniforms() {
-            let uniformCount = WebEngine.gl2.getProgramParameter(this.program, WebEngine.gl2.ACTIVE_UNIFORMS);
-            for (let i = 0; i < uniformCount; i++) {
-                let info = WebEngine.gl2.getActiveUniform(this.program, i);
+        Shader.prototype.detectUniforms = function () {
+            var uniformCount = WebEngine.gl2.getProgramParameter(this.program, WebEngine.gl2.ACTIVE_UNIFORMS);
+            for (var i = 0; i < uniformCount; i++) {
+                var info = WebEngine.gl2.getActiveUniform(this.program, i);
                 if (!info) {
                     break;
                 }
                 this.uniforms[info.name] = WebEngine.gl2.getUniformLocation(this.program, info.name);
             }
-        }
-    } // End class.
+        };
+        return Shader;
+    }()); // End class.
     WebEngine.Shader = Shader;
 })(WebEngine || (WebEngine = {})); // End namespace.
 //# sourceMappingURL=Shader.js.map
