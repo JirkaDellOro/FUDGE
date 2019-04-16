9aa0bc4e80618dc71d0fbd091b705299bbf11bbf
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-04-16 16:56:08 +0200

    some cleanup to host separate files

--- Core/src/Engine/WebGL-Jascha.ts
+++ Core/src/Engine/WebGL-Jascha.ts
@@ -1,83 +1,12 @@
 namespace Fudge {
-    export interface BufferSpecification {
-        size: number;   // The size of the datasample.
-        dataType: number; // The datatype of the sample (e.g. gl.FLOAT, gl.BYTE, etc.)
-        normalize: boolean; // Flag to normalize the data.
-        stride: number; // Number of indices that will be skipped each iteration.
-        offset: number; // Index of the element to begin with.
-    }
-    /*
-    interface MapNodeToRenderData {
-        [key: Node]: RenderData
-    }
-    */
-    interface RenderData {
-        shader: Shader;
-        material: Material;
-        mesh: Mesh;
-        doneTranformToWorld: boolean;
-    }
     interface ShaderProgram {
         webGLProgram: WebGLProgram;
         attributes: { [name: string]: number };
         uniforms: { [name: string]: WebGLUniformLocation };
     }
-    class Reference<T> {
-        private reference: T;
-        private count: number = 0;
 
-        constructor(_reference: T) {
-            this.reference = _reference;
-        }
-
-        public increaseCounter(): number {
-            this.count++;
-            return this.count;
-        }
-        public decreaseCounter(): number {
-            if (this.count == 0) throw (new Error("Negative reference counter"));
-            this.count--;
-            return this.count;
-        }
-    }
-    export class WebGL {
-        private canvas: HTMLCanvasElement; //offscreen render buffer
-        private crc3: WebGL2RenderingContext;
-        private programs: Map<Shader, Reference<WebGLProgram>> = new Map();
-        private parameters: Map<Material, Reference<WebGLVertexArrayObject>> = new Map();
-        private buffers: Map<Mesh, Reference<WebGLBuffer>> = new Map();
-        private nodes: Map<Node, RenderData> = new Map();
-
-        public addNode(_node: Node): void {
-            if (this.nodes.get(_node))
-                return;
-
-            let shader: Shader = (<ComponentMaterial>(_node.getComponents(ComponentMaterial)[0])).Material.Shader;
-            let rfrProgram: Reference<WebGLProgram>;
-            rfrProgram = this.programs.get(shader);
-            if (rfrProgram)
-                rfrProgram.increaseCounter();
-            else {
-                let program: WebGLProgram = this.createProgram(shader);
-                rfrProgram = new Reference<WebGLProgram>(program);
-                rfrProgram.increaseCounter();
-                this.programs.set(shader, rfrProgram);
-            }
-
-            let material: Material = (<ComponentMaterial>(_node.getComponents(ComponentMaterial)[0])).Material;
-            let rfrParameter: Reference<WebGLVertexArrayObject>;
-            rfrProgram = this.parameters.get(material);
-            if (rfrParameter)
-                rfrParameter.increaseCounter();
-            else {
-                let parameter: WebGLVertexArrayObject = this.createParameter(material);
-                rfrParameter = new Reference<WebGLVertexArrayObject>(parameter);
-                rfrParameter.increaseCounter();
-                this.parameters.set(material, rfrParameter);
-            }
-        }
-
-        //function createReference(_in: Map, )
+    class WebGL_Jascha {
+        crc3: WebGL2RenderingContext;
 
         private createProgram(_shader: Shader): ShaderProgram {
             let crc3: WebGL2RenderingContext = this.crc3;
@@ -154,10 +83,11 @@ namespace Fudge {
             let crc3: WebGL2RenderingContext = this.crc3;
             let bufferCreated: WebGLBuffer | null = crc3.createBuffer();
             if (bufferCreated === null)
-                return;
+                return null;
             let buffer: WebGLBuffer = bufferCreated;
             let vertexArrayObjectCreated: WebGLVertexArrayObject | null = crc3.createVertexArray();
-            if (vertexArrayObjectCreated === null) return;
+            if (vertexArrayObjectCreated === null)
+                return null;
             let vertexArrayObject: WebGLVertexArrayObject = vertexArrayObjectCreated;
             crc3.bindVertexArray(vertexArrayObject);
             crc3.bindBuffer(crc3.ARRAY_BUFFER, buffer);
