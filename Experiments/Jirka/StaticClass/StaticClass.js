cdad04af8f3112fc69c16cd0a5e4104a7771e9ad
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-05-02 23:24:47 +0200

    class register

---
+++ Experiments/Jirka/StaticClass/StaticClass.js
@@ -0,0 +1,36 @@
+"use strict";
+var StaticClass;
+(function (StaticClass) {
+    class Base {
+        static addClass(_class) {
+            return Base.classRegister.push(_class);
+        }
+    }
+    Base.classRegister = [];
+    class Sub1 extends Base {
+        constructor() {
+            super();
+            console.log("Hi, I'm an instance of Sub1");
+        }
+    }
+    Sub1.iRegister = Base.addClass(Sub1);
+    StaticClass.Sub1 = Sub1;
+    class Sub2 extends Base {
+        constructor() {
+            super();
+            console.log("Hi, I'm an instance of Sub2");
+        }
+    }
+    Sub2.iRegister = Base.addClass(Sub2);
+    StaticClass.Sub2 = Sub2;
+    for (let type of Base.classRegister)
+        console.log(type.name);
+    for (let i; i < 10; i++) {
+        let choice = Math.round(Math.random());
+        let typeChosen = Base.classRegister[choice];
+        console.log(typeChosen);
+        let instance = new typeChosen();
+        console.log(instance);
+    }
+})(StaticClass || (StaticClass = {}));
+//# sourceMappingURL=StaticClass.js.map
