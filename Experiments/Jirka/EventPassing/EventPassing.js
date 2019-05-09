561a0203e1d68cffb4f4a1b9ccbc66099930fe8a
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-05-09 23:27:13 +0200

    more tests and improvements on EventPassing

--- Experiments/Jirka/EventPassing/EventPassing.js
+++ Experiments/Jirka/EventPassing/EventPassing.js
@@ -3,12 +3,17 @@ var EventPassing;
 (function (EventPassing) {
     window.addEventListener("load", init);
     class EventPort extends MouseEvent {
+        constructor(type, _event) {
+            super(type, _event);
+            let target = _event.target;
+            this.canvasX = _event.clientX - target.getClientRects()[0].left;
+            this.canvasY = _event.clientY - target.getClientRects()[0].top;
+        }
     }
     class Port extends EventTarget {
         constructor() {
             super(...arguments);
             this.hndEvent = (_event) => {
-                // let event: EventPort = new EventPort(_event.type + "Port", _event);
                 let event = new EventPort(_event.type + "Port", _event);
                 this.dispatchEvent(event);
             };
@@ -18,11 +23,11 @@ var EventPassing;
         }
     }
     function init(_event) {
-        let element = document.querySelector("fieldset");
+        let element = document.querySelector("canvas");
         let port = new Port();
         port.connected = element;
-        port.processEvent("mouseup");
-        port.addEventListener("mouseupPort", hndPortEvent);
+        port.processEvent("mousemove");
+        port.addEventListener("mousemovePort", hndPortEvent);
     }
     function hndPortEvent(_event) {
         console.log("external", _event);
--- Experiments/Jirka/EventPassing/EventPassing.ts
+++ Experiments/Jirka/EventPassing/EventPassing.ts
@@ -1,7 +1,15 @@
 namespace EventPassing {
     window.addEventListener("load", init);
     class EventPort extends MouseEvent {
+        public canvasX: number;
+        public canvasY: number;
 
+        constructor(type: string, _event: MouseEvent) {
+            super(type, _event);
+            let target: HTMLElement = <HTMLElement>_event.target;
+            this.canvasX = _event.clientX - target.getClientRects()[0].left;
+            this.canvasY = _event.clientY - target.getClientRects()[0].top;
+        }
     }
 
     class Port extends EventTarget {
@@ -11,21 +19,20 @@ namespace EventPassing {
             this.connected.addEventListener(_type, this.hndEvent);
         }
 
-        public hndEvent = (_event: Event) => {
-            // let event: EventPort = new EventPort(_event.type + "Port", _event);
-            let event: EventPort = new EventPort(_event.type + "Port", _event);
+        public hndEvent: EventListener = (_event: Event) => {
+            let event: EventPort = new EventPort(_event.type + "Port", <MouseEvent>_event);
             this.dispatchEvent(event);
         }
     }
 
     function init(_event: Event): void {
-        let element: HTMLElement = document.querySelector("fieldset");
+        let element: HTMLElement = document.querySelector("canvas");
         let port: Port = new Port();
 
         port.connected = element;
-        port.processEvent("mouseup");
+        port.processEvent("mousemove");
 
-        port.addEventListener("mouseupPort", hndPortEvent);
+        port.addEventListener("mousemovePort", hndPortEvent);
     }
 
     function hndPortEvent(_event: Event): void {
