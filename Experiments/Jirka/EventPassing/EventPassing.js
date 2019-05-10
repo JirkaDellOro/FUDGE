75b552bc6e142496470b132b77326351445a904f
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-05-10 23:29:53 +0200

    EventPassing ready for implementation in FUDGE

--- Experiments/Jirka/EventPassing/EventPassing.js
+++ Experiments/Jirka/EventPassing/EventPassing.js
@@ -2,35 +2,61 @@
 var EventPassing;
 (function (EventPassing) {
     window.addEventListener("load", init);
-    class EventPort extends MouseEvent {
+    class PointerEventEx extends PointerEvent {
         constructor(type, _event) {
             super(type, _event);
             let target = _event.target;
-            this.canvasX = _event.clientX - target.getClientRects()[0].left;
-            this.canvasY = _event.clientY - target.getClientRects()[0].top;
+            this.clientRect = target.getClientRects()[0];
+            this.pointerX = _event.clientX - this.clientRect.left;
+            this.pointerY = _event.clientY - this.clientRect.top;
+        }
+    }
+    class KeyboardEventEx extends KeyboardEvent {
+        constructor(type, _event) {
+            super(type, _event);
         }
     }
     class Port extends EventTarget {
         constructor() {
             super(...arguments);
-            this.hndEvent = (_event) => {
-                let event = new EventPort(_event.type + "Port", _event);
+            this.hndPointerEvent = (_event) => {
+                let event = new PointerEventEx("ƒ" + _event.type, _event);
+                event.canvasX = this.connected.width * event.pointerX / event.clientRect.width;
+                event.canvasY = this.connected.height * event.pointerY / event.clientRect.height;
+                this.dispatchEvent(event);
+            };
+            this.hndKeyboardEvent = (_event) => {
+                let event = new KeyboardEventEx("ƒ" + _event.type, _event);
                 this.dispatchEvent(event);
             };
         }
-        processEvent(_type) {
-            this.connected.addEventListener(_type, this.hndEvent);
+        activatePointerEvent(_type, _on) {
+            this.activateEvent(this.connected, _type, this.hndPointerEvent, _on);
+        }
+        activateKeyboardEvent(_type, _on) {
+            this.activateEvent(this.connected.ownerDocument, _type, this.hndKeyboardEvent, _on);
+        }
+        activateEvent(_target, _type, _handler, _on) {
+            _type = _type.slice(1); // chip the ƒlorentin
+            if (_on)
+                _target.addEventListener(_type, _handler);
+            else
+                _target.removeEventListener(_type, _handler);
         }
     }
     function init(_event) {
         let element = document.querySelector("canvas");
         let port = new Port();
+        let input = document.querySelector("input");
+        input.addEventListener("keydown", hndEvent); // just for testing if two handler called
         port.connected = element;
-        port.processEvent("mousemove");
-        port.addEventListener("mousemovePort", hndPortEvent);
+        port.activatePointerEvent("\u0192pointerdown" /* DOWN */, true);
+        port.addEventListener("\u0192pointerdown" /* DOWN */, hndEvent);
+        port.activateKeyboardEvent("\u0192keyup" /* UP */, true);
+        port.addEventListener("\u0192keyup" /* UP */, hndEvent);
     }
-    function hndPortEvent(_event) {
-        console.log("external", _event);
+    function hndEvent(_event) {
+        console.log(_event);
     }
 })(EventPassing || (EventPassing = {}));
 //# sourceMappingURL=EventPassing.js.map
--- Experiments/Jirka/EventPassing/EventPassing.ts
+++ Experiments/Jirka/EventPassing/EventPassing.ts
@@ -1,41 +1,84 @@
 namespace EventPassing {
     window.addEventListener("load", init);
-    class EventPort extends MouseEvent {
+
+    const enum EVENT_KEYBOARD {
+        UP = "ƒkeyup",
+        DOWN = "ƒkeydown"
+    }
+    const enum EVENT_POINTER {
+        UP = "ƒpointerup",
+        DOWN = "ƒpointerdown"
+    }
+
+    class PointerEventEx extends PointerEvent {
+        public pointerX: number;
+        public pointerY: number;
         public canvasX: number;
         public canvasY: number;
+        public clientRect: ClientRect;
 
-        constructor(type: string, _event: MouseEvent) {
+        constructor(type: string, _event: PointerEventEx) {
             super(type, _event);
             let target: HTMLElement = <HTMLElement>_event.target;
-            this.canvasX = _event.clientX - target.getClientRects()[0].left;
-            this.canvasY = _event.clientY - target.getClientRects()[0].top;
+            this.clientRect = target.getClientRects()[0];
+            this.pointerX = _event.clientX - this.clientRect.left;
+            this.pointerY = _event.clientY - this.clientRect.top;
+        }
+    }
+
+    class KeyboardEventEx extends KeyboardEvent {
+        constructor(type: string, _event: KeyboardEventEx) {
+            super(type, _event);
         }
     }
 
     class Port extends EventTarget {
-        public connected: HTMLElement;
+        public connected: HTMLCanvasElement;
 
-        public processEvent(_type: string): void {
-            this.connected.addEventListener(_type, this.hndEvent);
+        public activatePointerEvent(_type: EVENT_POINTER, _on: boolean): void {
+            this.activateEvent(this.connected, _type, this.hndPointerEvent, _on);
+        }
+        public activateKeyboardEvent(_type: EVENT_KEYBOARD, _on: boolean): void {
+            this.activateEvent(this.connected.ownerDocument, _type, this.hndKeyboardEvent, _on);
         }
 
-        public hndEvent: EventListener = (_event: Event) => {
-            let event: EventPort = new EventPort(_event.type + "Port", <MouseEvent>_event);
+        public hndPointerEvent: EventListener = (_event: Event) => {
+            let event: PointerEventEx = new PointerEventEx("ƒ" + _event.type, <PointerEventEx>_event);
+            event.canvasX = this.connected.width * event.pointerX / event.clientRect.width;
+            event.canvasY = this.connected.height * event.pointerY / event.clientRect.height;
             this.dispatchEvent(event);
+        }
+
+        public hndKeyboardEvent: EventListener = (_event: Event) => {
+            let event: KeyboardEventEx = new KeyboardEventEx("ƒ" + _event.type, <KeyboardEventEx>_event);
+            this.dispatchEvent(event);
+        }
+
+        public activateEvent(_target: EventTarget, _type: string, _handler: EventListener, _on: boolean): void {
+            _type = _type.slice(1); // chip the ƒlorentin
+            if (_on)
+                _target.addEventListener(_type, _handler);
+            else
+                _target.removeEventListener(_type, _handler);
         }
     }
 
     function init(_event: Event): void {
-        let element: HTMLElement = document.querySelector("canvas");
+        let element: HTMLCanvasElement = document.querySelector("canvas");
         let port: Port = new Port();
+        let input: HTMLInputElement = document.querySelector("input");
+        input.addEventListener("keydown", hndEvent); // just for testing if two handler called
 
         port.connected = element;
-        port.processEvent("mousemove");
 
-        port.addEventListener("mousemovePort", hndPortEvent);
+        port.activatePointerEvent(EVENT_POINTER.DOWN, true);
+        port.addEventListener(EVENT_POINTER.DOWN, hndEvent);
+
+        port.activateKeyboardEvent(EVENT_KEYBOARD.UP, true);
+        port.addEventListener(EVENT_KEYBOARD.UP, hndEvent);
     }
 
-    function hndPortEvent(_event: Event): void {
-        console.log("external", _event);
+    function hndEvent(_event: Event): void {
+        console.log(_event);
     }
 }
