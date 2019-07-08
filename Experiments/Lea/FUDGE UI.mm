c0af4f4b9679e1dc6e712348454560b0fa16b2db
Lea <lea.stegk@hs-furtwangen.de>
2019-07-08 17:00:10 +0200

    deleted unnecessary data

---
+++ Experiments/Lea/FUDGE UI.mm
@@ -0,0 +1,43 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1562586625098" ID="ID_663824247" MODIFIED="1562596203049" TEXT="FUDGE UI Funktionen">
+<node CREATED="1562592934168" HGAP="24" ID="ID_1819286157" MODIFIED="1562593368576" POSITION="right" TEXT="Scene Editor" VSHIFT="-49">
+<node CREATED="1562592948142" ID="ID_939958157" MODIFIED="1562594547719" TEXT="Oberste Bedienebene">
+<node CREATED="1562594001819" HGAP="18" ID="ID_1254664403" MODIFIED="1562594397513" TEXT="Menu bar: " VSHIFT="134">
+<node CREATED="1562594057554" FOLDED="true" ID="ID_1568087724" MODIFIED="1562594614313" TEXT="File: New, Open, Save, Save as, Settings">
+<node CREATED="1562594580582" ID="ID_14698085" MODIFIED="1562594611690" TEXT="Settings: Shortcuts, Themes"/>
+</node>
+<node CREATED="1562594186250" ID="ID_443641311" MODIFIED="1562594295524" TEXT="Edit: Undo, Redo, Copy, Paste, Select all, Deselect"/>
+<node CREATED="1562594297491" ID="ID_1555723413" MODIFIED="1562597085478" TEXT="Window: Zoom In, Zoom Out, Animation Editor, Sketch Editor"/>
+<node CREATED="1562594401763" ID="ID_1052766371" MODIFIED="1562594547719" TEXT="Tools: Objekt rotieren, skalieren, bewegen, ausw&#xe4;hlen, Ansicht bewegen"/>
+<node CREATED="1562594557380" ID="ID_1045180573" MODIFIED="1562594576358" TEXT="Help: About, Tutorials"/>
+</node>
+<node CREATED="1562593175545" HGAP="15" ID="ID_351410150" MODIFIED="1562594008499" TEXT="Toolbar: Objekt rotieren, skalieren, bewegen, ausw&#xe4;hlen, Ansicht bewegen" VSHIFT="37"/>
+<node CREATED="1562593528392" HGAP="5" ID="ID_1396702803" MODIFIED="1562593611606" TEXT="Scene Tree: Reihenfolge/Beziehung zwischen Objekten &#xe4;ndern, Neues Objekt erstellen, Objekt l&#xf6;schen" VSHIFT="35"/>
+<node CREATED="1562593622115" HGAP="-2" ID="ID_558906421" MODIFIED="1562593966948" TEXT="Viewport: Optische Anordnung der Elemente, Benutzung der Tools" VSHIFT="33"/>
+<node CREATED="1562593685258" HGAP="-6" ID="ID_69147282" MODIFIED="1562596360115" TEXT="Inspector: &#xc4;nderung der Objekteigenschaften, Materialien, Skripte, Audio, Rigidbodies" VSHIFT="19">
+<node CREATED="1562593805334" ID="ID_1579061459" MODIFIED="1562593820660" TEXT="Objekteigenschaften"/>
+<node CREATED="1562593820661" ID="ID_1655756880" MODIFIED="1562593829459" TEXT="Material"/>
+<node CREATED="1562593857808" ID="ID_1527820368" MODIFIED="1562593866750" TEXT="Skript"/>
+<node CREATED="1562593867976" ID="ID_513509845" MODIFIED="1562593872688" TEXT="Audio"/>
+<node CREATED="1562593873853" ID="ID_1235108072" MODIFIED="1562593939276" TEXT="Rigidbody"/>
+</node>
+</node>
+</node>
+<node CREATED="1562593008199" HGAP="21" ID="ID_1602607747" MODIFIED="1562597085478" POSITION="left" TEXT="Animation Editor" VSHIFT="-54">
+<node CREATED="1562594644060" ID="ID_267490870" MODIFIED="1562594684013" TEXT="Toolbar: Keyframe, Event, Label"/>
+<node CREATED="1562594684845" ID="ID_331432028" MODIFIED="1562595988145" TEXT="Inspector Dopesheet: Animationsname, SPS, FPS, Playmode, First frame, frame back, play, stop, frame forward, last frame,     Objekte und animierbare Eigenschaften, Eigenschaft hinzuf&#xfc;gen"/>
+<node CREATED="1562595281434" ID="ID_908326199" MODIFIED="1562595306577" TEXT="Inspector Curve View:"/>
+<node CREATED="1562595376169" ID="ID_1340251796" MODIFIED="1562595974554" TEXT="Dopesheet / Curve View: Zeitleiste, Frames, Anzeige Keyframes/Curve, Labels, Events,  /  Kurve mithilfe von Tangenten &#xe4;ndern, Tangenten Werte zuweisen"/>
+</node>
+<node CREATED="1562593097842" ID="ID_1718444650" MODIFIED="1562597471039" POSITION="left" TEXT="Sketch Editor" VSHIFT="21">
+<node CREATED="1562596031091" ID="ID_1809766605" MODIFIED="1562596347327" TEXT="Toolbar: Neue Form, ausw&#xe4;hlen, Ansicht bewegen, Neuer Pfad, Rotieren, skalieren, bewegen, kombinieren, alignment"/>
+<node CREATED="1562596503109" ID="ID_525858882" MODIFIED="1562596521358" TEXT="Canvas: Optische Anordnung der Formen"/>
+<node CREATED="1562596539329" ID="ID_848839315" MODIFIED="1562596818832" TEXT="Inspector: Name, fill color, line color, line width, closed, order"/>
+<node COLOR="#cc3300" CREATED="1562597400234" ID="ID_1380835573" MODIFIED="1562597466020" TEXT="Order in Layer umbenennen damit Interface an Scene Editor angeglichen wird?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1562593401764" HGAP="-29" ID="ID_382361001" MODIFIED="1562595213787" POSITION="right" TEXT="Modelling Editor" VSHIFT="1"/>
+</node>
+</map>
