09bfcf486f562a18854296c719af44d67ac7de0d
Monika <monika.galkewitsch@hs-furtwangen.de>
2019-08-06 19:27:50 +0200

    Merge Conflics ACTUALLY FIXED THIS TIME I SWEAR

--- Experiments/Monika/UI_System_Experimental/scr/SimpleComponent.js
+++ Experiments/Monika/UI_System_Experimental/scr/SimpleComponent.js
@@ -1,13 +1,9 @@
-var GoldenLayoutTest;
-(function (GoldenLayoutTest) {
+var UI;
+(function (UI) {
     class SimpleComponent {
         constructor(container, state) {
-            let element = document.createElement("div");
-            let mutator = { people: [{ name: "Lukas", age: 24 }, { name: "Jirka", age: 54 }], cars: [{ brand: "Audi", km: 20000, new: false }, { brand: "VW", km: 100000, new: true }] };
-            GoldenLayoutTest.UIGenerator.createFromMutator(mutator, element);
-            container.getElement().html(element);
         }
     }
-    GoldenLayoutTest.SimpleComponent = SimpleComponent;
-})(GoldenLayoutTest || (GoldenLayoutTest = {}));
+    UI.SimpleComponent = SimpleComponent;
+})(UI || (UI = {}));
 //# sourceMappingURL=SimpleComponent.js.map
--- Experiments/Monika/UI_System_Experimental/scr/app.ts
+++ Experiments/Monika/UI_System_Experimental/scr/app.ts
@@ -1,8 +1,18 @@
+/// <reference path="../../../../Core/build/Fudge.d.ts"/>
+import ƒ = Fudge;
+
+
 namespace UI {
     let myLayout: GoldenLayout;
     let savedState: string;
-    // let file:HTML = "test.html"
-    let config: GoldenLayout.Config = {
+
+    let canvas: HTMLCanvasElement;
+    let viewPort: ƒ.Viewport = new ƒ.Viewport();
+    let camera: ƒ.Node;
+    window.addEventListener("load", init);
+
+    function init(){
+        let config: GoldenLayout.Config = {
         content: [{
             type: 'row',
             content: [{
@@ -17,15 +27,51 @@ namespace UI {
             }]
         }]
     };
-    function createSimpleComponent (container: any, state: any) {
-        // return SimpleComponent.create(container, state);
-        return new SimpleComponent(container, state);
-    }
+
+
+    initViewport()
     myLayout = new GoldenLayout(config);
-    myLayout.registerComponent('Inspector', createSimpleComponent);
-    myLayout.registerComponent('Viewport', createSimpleComponent);
-    console.log("I work");
+    myLayout.registerComponent('Inspector', createCameraComponent);
+    myLayout.registerComponent('Viewport', createViewportComponent);
 
     myLayout.init();
+}
+    function initViewport(){
+        // create asset
+        let branch: ƒ.Node = Scenes.createAxisCross();
+        branch.addComponent(new ƒ.ComponentTransform());
+
+        // initialize RenderManager and transmit content
+        ƒ.RenderManager.initialize();
+        ƒ.RenderManager.addBranch(branch);
+        ƒ.RenderManager.update();
+
+        // initialize viewports
+        canvas = document.createElement("canvas");
+        document.body.append(canvas);
+        camera = Scenes.createCamera(new ƒ.Vector3(1, 2, 3));
+        let cmpCamera: ƒ.ComponentCamera = camera.getComponent(ƒ.ComponentCamera);
+        viewPort.initialize(canvas.id, branch, cmpCamera, canvas);
+        viewPort.adjustingFrames = true;
+        ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, animate);
+        ƒ.Loop.start();
+        function animate(_event: Event): void {
+
+            branch.cmpTransform.local.rotateY(1);
+            ƒ.RenderManager.update();
+            // prepare and draw viewport
+            viewPort.draw();
+        }
+    }
+    function createViewportComponent (container: any, state: any) {
+        container.getElement().append(canvas);
+    }
+
+    function createCameraComponent (container:any, state:any)
+    {
+        return new CameraUI(container, state, camera.getComponent(ƒ.ComponentCamera));
+    }
+
+
 
 }
