bb2b733f5429fe4f9206ab405ee639482c48d8c6
Lukas Scheuerle <lukas.scheuerle@web.de>
2019-08-09 14:12:39 +0200

    added Time Class to ComponentAnimator

--- Core/src/Component/ComponentAnimator.ts
+++ Core/src/Component/ComponentAnimator.ts
@@ -40,10 +40,15 @@ namespace Fudge {
     playmode: ANIMATION_PLAYMODE;
     playback: ANIMATION_PLAYBACK;
     events: AnimationEventTrigger;
+    time: Time;
+    speedScalesWithGlobalSpeed: boolean = true;
 
+    private speedScale: number = 1;
     private lastTime: number = 0;
-    private startTime: number = 0;
-    private timeAtStart: number = 0;
+    private lastFrameTime: number = 0;
+    private lastDirection: number = 0;
+    // private startTime: number = 0;
+    // private timeAtStart: number = 0;
 
     constructor(_animation: Animation, _playmode: ANIMATION_PLAYMODE, _playback: ANIMATION_PLAYBACK) {
       super();
@@ -51,21 +56,31 @@ namespace Fudge {
       this.playmode = _playmode;
       this.playback = _playback;
 
+      this.time = new Time();
+
       //TODO: update animation total time when loading a different animation?
       this.animation.calculateTotalTime();
-      this.lastTime = 0;
+      // this.lastTime = 0;
       //TODO: get an individual time class to work with.
       this.jumpTo(0, Date.now()); // <-- remove this when time class is implemented
       //TODO: register updateAnimatioStart() properly into the gameloop
       Loop.addEventListener(EVENT.LOOP_FRAME, this.updateAnimationLoop.bind(this));
+      Time.game.addEventListener(EVENT.TIME_SCALED, this.updateScale.bind(this));
+    }
+
+    set speed(_s: number) {
+      this.speedScale = _s;
+      this.updateScale();
     }
 
     jumpTo(_time: number, _currentTime: number): void {
       _time = this.calculateCurrentTime(_time, this.calculateDirection(_time));
-      this.startTime = _time;
-      this.timeAtStart = _currentTime;
-      if (this.playback != ANIMATION_PLAYBACK.FRAMEBASED)
-        this.lastTime = _currentTime;
+      //TODO: maybe this can be outsourced to the time class as well.
+      this.time.set(_time);
+      // this.startTime = _time;
+      // this.timeAtStart = _currentTime;
+      // if (this.playback != ANIMATION_PLAYBACK.FRAMEBASED)
+      //   this.lastTime = _currentTime;
     }
 
     //#region updateAnimation
@@ -84,17 +99,17 @@ namespace Fudge {
     }
 
     private updateAnimationContinous(): void {
-      //TODO: use own time class
-      let direction: number = this.calculateDirection(Date.now());
-      let time: number = this.calculateCurrentTime(Date.now(), direction);
+      let time: number = this.time.get();
+      let direction: number = this.calculateDirection(time);
+      time = this.calculateCurrentTime(time, direction);
 
       this.updateAnimation(time, direction);
     }
 
     private updateAnimationRastered(): void {
-      //TODO: use own time class
+      let time: number = this.time.get();
       let direction: number = this.calculateDirection(Date.now());
-      let time: number = this.calculateCurrentTime(Date.now(), direction);
+      time = this.calculateCurrentTime(Date.now(), direction);
       time = time - (time % (1000 / this.animation.fps));
 
       //TODO: possible optimisation: only update animation if next Frame has been reached
@@ -103,7 +118,7 @@ namespace Fudge {
 
     private updateAnimationFramebased(): void {
       let timePerFrame: number = 1000 / this.animation.fps;
-      let time: number = this.lastTime + timePerFrame;
+      let time: number = this.lastFrameTime + timePerFrame;
       let direction: number = this.calculateDirection(time);
       time = time % this.animation.totalTime;
 
@@ -114,20 +129,21 @@ namespace Fudge {
       let mutator: Mutator = this.animation.getMutated(_time, _direction);
       this.getContainer().applyAnimation(mutator);
       this.checkEvents(_time, _direction);
-      this.lastTime = _time;
+      // this.lastTime = _time;
     }
     //#endregion
 
     private calculateCurrentTime(_time: number, _direction: number): number {
-      let time: number = ((_time - this.timeAtStart) * _direction + this.startTime) % this.animation.totalTime;
+      // let time: number = ((_time - this.timeAtStart) * _direction + this.startTime) % this.animation.totalTime;
+      let time: number = _time % this.animation.totalTime;
       if (_direction < 0) {
-        time += this.animation.totalTime;
+        time = this.animation.totalTime - time;
       }
       return time;
     }
 
     private calculateDirection(_time: number): number {
-      _time = _time + this.startTime - this.timeAtStart;
+      // _time = _time + this.startTime - this.timeAtStart;
       switch (this.playmode) {
         case ANIMATION_PLAYMODE.STOP:
           return 0;
@@ -148,13 +164,60 @@ namespace Fudge {
       }
     }
 
+    private updateScale(): void {
+      let newScale: number = this.speedScale;
+      if (this.speedScalesWithGlobalSpeed)
+        newScale *= Time.game.getScale();
+      this.time.setScale(newScale);
+    }
+
     private checkEvents(_time: number, _direction: number): void {
-      //TODO Catch Events at the beginning & end of the animation
       if (this.playmode == ANIMATION_PLAYMODE.STOP || _direction == 0)
         return;
+      if (this.playmode == ANIMATION_PLAYMODE.PINGPONG) {
+        if (this.lastDirection == _direction) {
+          if (_direction > 0) {
+            this.checkEventBetween(this.lastTime, _time);
+          } else {
+            this.checkEventBetween(_time, this.lastTime);
+          }
+        } else {
+          if (_direction > 0) {
+            this.checkEventBetween(this.lastTime, this.animation.totalTime);
+            this.checkEventBetween(_time, this.animation.totalTime);
+          } else {
+            this.checkEventBetween(0, this.lastTime);
+            this.checkEventBetween(0, this.animation.totalTime);
+          }
+        }
+      }
+      else if (0 < _direction) {
+        if (this.lastTime < _time) {
+          this.checkEventBetween(this.lastTime, _time);
+        } else {
+          this.checkEventBetween(this.lastTime, this.animation.totalTime);
+          this.checkEventBetween(0, _time);
+        }
+      } else {
+        if (_time < this.lastTime) {
+          this.checkEventBetween(_time, this.lastTime);
+        } else {
+          this.checkEventBetween(0, this.lastTime);
+          this.checkEventBetween(_time, this.animation.totalTime);
+        }
+      }
+
+      this.lastTime = _time;
+    }
+
+    private checkEventBetween(_min: number, _max: number): void {
+      if (_min > _max) {
+        let t: number = _min;
+        _min = _max;
+        _max = t;
+      }
       for (let name in this.events) {
-        if (_direction > 0 && this.lastTime < this.events[name] && this.events[name] < _time
-          || _direction < 0 && this.lastTime > this.events[name] && this.events[name] > _time) {
+        if (_min < this.events[name] && this.events[name] < _max) {
           this.dispatchEvent(new Event(name));
         }
       }
