63e59d701fca5af0e9f633440927feda6906c093
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-08-12 18:37:33 +0200

    Head is out... viewport in Electron!

--- Test/Electron/IncludeFudge/IncludeFudge.ts
+++
@@ -1,8 +0,0 @@
-namespace IncludeFudge {
-
-    export class ClassMain {
-        public constructor() {
-            console.log(this.constructor.name, this);
-        }
-    }
-}
---
+++ Test/Electron/Viewport/Render.js
@@ -0,0 +1,23 @@
+var ElectronViewport;
+(function (ElectronViewport) {
+    var ƒ = Fudge;
+    window.addEventListener("DOMContentLoaded", init);
+    function init() {
+        // create asset
+        let branch = Scenes.createAxisCross();
+        // initialize RenderManager and transmit content
+        ƒ.RenderManager.initialize();
+        ƒ.RenderManager.addBranch(branch);
+        ƒ.RenderManager.update();
+        // initialize viewport
+        let camera = Scenes.createCamera(new ƒ.Vector3(3, 3, 5));
+        let cmpCamera = camera.getComponent(ƒ.ComponentCamera);
+        cmpCamera.projectCentral(1, 45);
+        let canvas = Scenes.createCanvas();
+        document.body.appendChild(canvas);
+        let viewPort = new ƒ.Viewport();
+        viewPort.initialize("TestViewport", branch, cmpCamera, canvas);
+        viewPort.draw();
+    }
+})(ElectronViewport || (ElectronViewport = {}));
+//# sourceMappingURL=Render.js.map
