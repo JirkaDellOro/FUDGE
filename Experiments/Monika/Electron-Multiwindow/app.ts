e173f8e0fd1a7edfee6ed87368bd133cdef3dfbf
Monika <monika.galkewitsch@hs-furtwangen.de>
2019-08-12 12:44:41 +0200

    Multiwindow Experiment

---
+++ Experiments/Monika/Electron-Multiwindow/app.ts
@@ -0,0 +1,23 @@
+/// <reference path="node_modules/electron/electron.d.ts"/>
+// import {app, BrowserWindow, ipcMain, ipcRenderer} from 'electron'
+let electron = require('electron');
+let ipcRenderer = electron.ipcRenderer;
+
+let windowButton: HTMLButtonElement = document.createElement("button");
+windowButton.innerHTML = "Open up!";
+windowButton.addEventListener("click", sendWindowRequest);
+
+window.addEventListener("DOMContentLoaded", start);
+function start(_event: Event): void {
+  document.body.append(windowButton);
+}
+function sendWindowRequest() {
+  ipcRenderer.send('open-up');
+}
+// console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+// ipcRenderer.on('asynchronous-reply', (event, arg) => {
+//   console.log(arg) // prints "pong"
+// })
+// ipcRenderer.send('asynchronous-message', 'ping')
+
