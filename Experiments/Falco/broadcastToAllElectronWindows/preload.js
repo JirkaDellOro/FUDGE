f240a77d0971f33eb343a321d5dac638e8088dc5
Falco Böhnke <falco.boehnke@gmail.com>
2019-08-12 14:03:11 +0200

    Add files via upload

---
+++ Experiments/Falco/broadcastToAllElectronWindows/preload.js
@@ -0,0 +1,13 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', function () {
+    var replaceText = function (selector, text) {
+        var element = document.getElementById(selector);
+        if (element)
+            element.innerText = text;
+    };
+    for (var _i = 0, _a = ['chrome', 'node', 'electron']; _i < _a.length; _i++) {
+        var type = _a[_i];
+        replaceText(type + "-version", process.versions[type]);
+    }
+});
