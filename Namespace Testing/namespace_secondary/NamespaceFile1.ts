6046eab2bd495f554863e0a92b98d57761956d77
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-08-12 17:18:43 +0200

    Relocate namespaceTesting von Falco

--- Namespace Testing/namespace_secondary/NamespaceFile1.ts
+++
@@ -1,7 +0,0 @@
-namespace  nsR{
-    export class class2{
-        public testlog(){
-            console.log("class 2");
-        }
-    }
-}
--- Namespace Testing/namespace_secondary/NamespaceFile2.ts
+++
@@ -1,7 +0,0 @@
-namespace  nsR{
-    export class class3{
-        public testlog(){
-            console.log("class 3");
-        }
-    }
-}
