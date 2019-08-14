e680c794e14d54a9e8515dbd8e49b9de9fe4c41c
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-08-14 15:47:01 +0200

    added missing files

--- Test/Electron/IncludeFudge/main.js
+++ Test/Electron/IncludeFudge/main.js
@@ -47,7 +47,7 @@ app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
-})
+}) 
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
