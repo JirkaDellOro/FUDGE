5cc827a069b662dbfa352e9744399fbcbdb8db2d
Thomas Dorner <TDorner@users.noreply.github.com>
2019-08-15 16:44:05 +0200

    Added missing Audio Components
    - ComponentAudio 
    - ComponentAudioListener (WIP)

---
+++ Core/Source/Component/ComponentAudioListener.ts
@@ -0,0 +1,11 @@
+/// <reference path="Component.ts"/>
+namespace Fudge {
+    /**
+     * Attaches a [[AudioListener]] to the node
+     * @authors Thomas Dorner, HFU, 2019
+     */
+    export class ComponentAudioListener extends Component {
+
+        
+    }
+}
