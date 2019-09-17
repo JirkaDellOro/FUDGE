5ec78f08aba401ae1661510dcc8422f29ea86e1d
Wowaiki <wowaiki@googlemail.com>
2019-09-17 10:14:38 +0200

    Add files via upload

--- Test/Raycast Picker/Raycast_test_object/picking.ts
+++ Test/Raycast Picker/Raycast_test_object/picking.ts
@@ -3,13 +3,15 @@
 import * as THREE from "./Build/three";
 import { Vector2 } from "./src/math/Vector2";
 import { raycast } from "./raycast";
-/// <reference path="Build/three"/>
+import { ThreeMFLoader } from "./examples/jsm/loaders/3MFLoader";
+
 
 
 let renderer, scene;
 let group, camera;
 let mouseVector: Vector2;
 let sprite;
+//let THREE: THREE;
 init();
 animate();
 
@@ -19,79 +21,79 @@ animate();
 
 
 
-function init() {
+function init(): void {
 
 
 
 
-	// init renderer
-	renderer = new THREE.WebGLRenderer({ antialias: true });
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
+  // init renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-	// init scene
-	scene = new THREE.Scene();
-	scene.background = THREE.Color(0xffffff);
+  // init scene
+  scene = new THREE.Scene();
+  scene.background = THREE.Color(0xffffff);
 
-	// init camera
-	camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.set(15, 15, 15);
-	camera.lookAt(scene.position);
+  // init camera
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(15, 15, 15);
+  camera.lookAt(scene.position);
 
-	group = new THREE.Group();
-	scene.add(group);
+  group = new THREE.Group();
+  scene.add(group);
 
 
 
-	/*  var radius = 6;
+  /*  var radius = 6;
 	  var height = 8;
 	  var segments = 16;
 	  var geometry = new THREE.ConeBufferGeometry(radius, height, segments);
 	 // geometry.position.set(15,15,15);
 	  group.add( geometry );
 	  */
-	sprite = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f' }));
-	sprite.position.set(6, 5, 5);
-	sprite.scale.set(2, 5, 1);
-	sprite.center;
+  sprite = new THREE.Sprite(new THREE.SpriteMaterial({ color: "#69f" }));
+  sprite.position.set(6, 5, 5);
+  sprite.scale.set(2, 5, 1);
+ // sprite.center;
 
-	group.add(sprite);
-
-
-	window.addEventListener('resize', this.onWindowResize, false);
-	// window.addEventListener( "mousemove", onDocumentMouseMove, false );
-	window.addEventListener("mousedown", this.onDocumentMouseDown, false);
-	//  window.addEventListener("mouseup", onDocumentMouseUp, false);
-
-}
+  group.add(sprite);
 
 
-
-
-function animate() {
-
-	renderer.render(scene, camera);
-	requestAnimationFrame(animate);
+  //window.addEventListener("resize", this.onWindowResize, false);
+  // window.addEventListener( "mousemove", onDocumentMouseMove, false );
+ // window.addEventListener("mousedown", this.onDocumentMouseDown, false);
+  //  window.addEventListener("mouseup", onDocumentMouseUp, false);
 
 }
 
+
+
+
+function animate(): void {
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+
+}
+/*
 function onWindowResize() {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-	renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-}
+}*/
 
 
-
+/*
 function onDocumentMouseDown(event) {
-	let raycastObject = new raycast(mouseVector, this.camera);
-	console.log(raycastObject.getIntersects);
+  let raycastObject = new raycast(mouseVector, this.camera);
+  console.log(raycastObject.hitTest());
 
-}
+}*/
 
 
 
