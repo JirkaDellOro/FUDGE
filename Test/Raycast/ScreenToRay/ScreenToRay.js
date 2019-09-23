124f7baa5220d9d990e7c73b4d18beeff95ee233
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-09-23 12:57:01 +0200

    Started on Ray!

---
+++ Test/Raycast/ScreenToRay/ScreenToRay.js
@@ -0,0 +1,164 @@
+var ScreenToRay;
+(function (ScreenToRay) {
+    var ƒ = FudgeCore;
+    window.addEventListener("load", init);
+    let uiMaps = {};
+    let uiClient;
+    let canvas;
+    let viewPort = new ƒ.Viewport();
+    let camera;
+    let uiCamera;
+    let mouse = new ƒ.Vector2();
+    function init() {
+        // create asset
+        let branch = Scenes.createAxisCross();
+        branch.addComponent(new ƒ.ComponentTransform());
+        // initialize RenderManager and transmit content
+        ƒ.RenderManager.initialize();
+        ƒ.RenderManager.addBranch(branch);
+        ƒ.RenderManager.update();
+        // initialize viewports
+        canvas = document.getElementsByTagName("canvas")[0];
+        camera = Scenes.createCamera(new ƒ.Vector3(1, 2, 3));
+        let cmpCamera = camera.getComponent(ƒ.ComponentCamera);
+        viewPort.initialize(canvas.id, branch, cmpCamera, canvas);
+        canvas.addEventListener("mousemove", setCursorPosition);
+        let menu = document.getElementsByTagName("div")[0];
+        menu.innerHTML = "Test automatic rectangle transformation. Adjust CSS-Frame and framings";
+        uiCamera = new UI.Camera();
+        menu.appendChild(uiCamera);
+        appendUIScale(menu, "DestinationToSource", viewPort.frameDestinationToSource);
+        appendUIComplex(menu, "CanvasToDestination", viewPort.frameCanvasToDestination);
+        appendUIScale(menu, "ClientToCanvas", viewPort.frameClientToCanvas);
+        uiClient = new UI.Rectangle("ClientRectangle");
+        uiClient.addEventListener("input", hndChangeOnClient);
+        menu.appendChild(uiClient);
+        update();
+        uiCamera.addEventListener("input", hndChangeOnCamera);
+        setCamera();
+        viewPort.adjustingFrames = true;
+        logMutatorInfo("Camera", cmpCamera);
+        for (let name in uiMaps) {
+            logMutatorInfo(name, uiMaps[name].framing);
+        }
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, animate);
+        ƒ.Loop.start();
+        function animate(_event) {
+            update();
+            branch.cmpTransform.local.rotateY(1);
+            ƒ.RenderManager.update();
+            // prepare and draw viewport
+            //viewPort.prepare();
+            viewPort.draw();
+            computeRay();
+        }
+    }
+    function computeRay() {
+        //
+    }
+    function setCursorPosition(_event) {
+        mouse = new ƒ.Vector2(_event.clientX, _event.clientY);
+        ƒ.Debug.info(mouse.get());
+    }
+    function logMutatorInfo(_title, _mutable) {
+        let mutator = _mutable.getMutator();
+        let types = _mutable.getMutatorAttributeTypes(mutator);
+        console.group(_title);
+        console.log("Types: ", types);
+        console.log("Mutator: ", mutator);
+        console.groupEnd();
+    }
+    function appendUIComplex(_parent, _name, _framing) {
+        let uiMap = new UI.FramingComplex(_name);
+        uiMap.addEventListener("input", hndChangeOnComplex);
+        _parent.appendChild(uiMap);
+        uiMaps[_name] = { ui: uiMap, framing: _framing };
+    }
+    function appendUIScale(_parent, _name, _framing) {
+        let uiMap = new UI.FramingScaled(_name);
+        uiMap.addEventListener("input", hndChangeOnScale);
+        _parent.appendChild(uiMap);
+        uiMaps[_name] = { ui: uiMap, framing: _framing };
+    }
+    function hndChangeOnComplex(_event) {
+        let target = _event.currentTarget;
+        setRectComplex(target);
+    }
+    function hndChangeOnScale(_event) {
+        let target = _event.currentTarget;
+        setRectScale(target);
+    }
+    function hndChangeOnCamera(_event) {
+        //let target: UI.Rectangle = <UI.Rectangle>_event.currentTarget;
+        setCamera();
+    }
+    function hndChangeOnClient(_event) {
+        let target = _event.currentTarget;
+        setClient(target);
+    }
+    function setRectComplex(_uiMap) {
+        let value = _uiMap.get();
+        let framing = uiMaps[_uiMap.name].framing;
+        for (let key in value) {
+            switch (key) {
+                case "Margin":
+                    framing.margin = value[key];
+                    break;
+                case "Padding":
+                    framing.padding = value[key];
+                    break;
+                case "Result":
+                    break;
+                default:
+                    throw (new Error("Invalid name: " + key));
+            }
+        }
+    }
+    function setRectScale(_uiMap) {
+        let value = _uiMap.get();
+        let framing = uiMaps[_uiMap.name].framing;
+        framing.setScale(value.normWidth, value.normHeight);
+    }
+    function setCamera() {
+        let params = uiCamera.get();
+        let cmpCamera = camera.getComponent(ƒ.ComponentCamera);
+        cmpCamera.projectCentral(params.aspect, params.fieldOfView);
+    }
+    function setClient(_uiRectangle) {
+        let rect = _uiRectangle.get();
+        canvas.style.left = rect.x + "px";
+        canvas.style.top = rect.y + "px";
+        canvas.style.width = rect.width + "px";
+        canvas.style.height = rect.height + "px";
+    }
+    function update() {
+        for (let name in uiMaps) {
+            // uiMap.ui.set({ Margin: uiMap.map.margin, Padding: uiMap.map.padding });
+            switch (name) {
+                case "ClientToCanvas": {
+                    let uiMap = uiMaps[name];
+                    uiMap.ui.set(uiMap.framing);
+                    uiMap.ui.set({ Result: viewPort.getCanvasRectangle() });
+                    break;
+                }
+                case "CanvasToDestination": {
+                    let uiMap = uiMaps[name];
+                    uiMap.ui.set({ Margin: uiMap.framing.margin, Padding: uiMap.framing.padding });
+                    uiMap.ui.set({ Result: viewPort.rectDestination });
+                    break;
+                }
+                case "DestinationToSource": {
+                    let uiMap = uiMaps[name];
+                    uiMap.ui.set(uiMap.framing);
+                    uiMap.ui.set({ Result: viewPort.rectSource });
+                    break;
+                }
+            }
+        }
+        let clientRect = canvas.getBoundingClientRect();
+        uiClient.set({ x: clientRect.left, y: clientRect.top, width: clientRect.width, height: clientRect.height });
+        let cmpCamera = camera.getComponent(ƒ.ComponentCamera);
+        uiCamera.set({ aspect: cmpCamera.getAspect(), fieldOfView: cmpCamera.getFieldOfView() });
+    }
+})(ScreenToRay || (ScreenToRay = {}));
+//# sourceMappingURL=ScreenToRay.js.map
