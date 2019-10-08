f8a82ad708a6271c21acdc11b51d2d0feebd7eb4
Monika <monika.galkewitsch@hs-furtwangen.de>
2019-10-08 17:00:26 +0200

    deleting useless garbage

--- Editor/Source/Fudge/NodePanel.ts
+++
@@ -1,8 +0,0 @@
-///<reference types="../../../Core/Build/FudgeCore"/>
-///<reference types="../../Examples/Code/Scenes"/>
-
-namespace Fudge {
-    import ƒ = FudgeCore;
-    import ƒui = FudgeUserInterface;
-   
-}
