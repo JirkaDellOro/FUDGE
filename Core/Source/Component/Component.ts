658f8e0555e224eaa4065cc876a0206a478cd9ea
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-10-22 15:09:46 +0200

    no component container in mutator, jsdoc on viewport fixed

--- Core/Source/Component/Component.ts
+++ Core/Source/Component/Component.ts
@@ -63,6 +63,7 @@ namespace FudgeCore {
 
         protected reduceMutator(_mutator: Mutator): void {
             delete _mutator.singleton;
+            delete _mutator.container;
         }
         //#endregion
     }
