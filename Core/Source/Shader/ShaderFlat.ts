e7aa99408806fafb42bde28bca1a8b9fed146e22
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-11-11 23:26:32 +0100

    FlatShader: material color integrated

--- Core/Source/Shader/ShaderFlat.ts
+++ Core/Source/Shader/ShaderFlat.ts
@@ -49,11 +49,12 @@ namespace FudgeCore {
             return `#version 300 es
                     precision mediump float;
 
+                    uniform vec4 u_color;
                     flat in vec4 v_color;
                     out vec4 frag;
                     
                     void main() {
-                        frag = v_color;
+                        frag = u_color * v_color;
                     }`;
         }
     }
