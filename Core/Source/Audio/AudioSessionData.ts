1e684a8ae70deafc3f39576225e964e337940046
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-12-01 09:47:42 +0100

    one _error missed->fixed

--- Core/Source/Audio/AudioSessionData.ts
+++ Core/Source/Audio/AudioSessionData.ts
@@ -66,7 +66,7 @@ namespace FudgeCore {
                     this.pushBufferInArray(_url, decodedAudio);
                     return decodedAudio;
                 } catch (_error) {
-                    this.logErrorFetch(e);
+                    this.logErrorFetch(_error);
                     return null;
                 }
             } 
