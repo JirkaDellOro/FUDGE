7f2edde53605cc74d309f633b6e1301b3171eda9
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-12-07 13:37:37 +0100

    Normals normalized after application of worldmatrix

--- Core/Source/Shader/ShaderFlat.ts
+++ Core/Source/Shader/ShaderFlat.ts
@@ -33,7 +33,7 @@ namespace FudgeCore {
                     
                     void main() {   
                         gl_Position = u_projection * vec4(a_position, 1.0);
-                        vec3 normal = mat3(u_world) * a_normal;
+                        vec3 normal = normalize(mat3(u_world) * a_normal);
 
                         v_color = u_ambient.color;
                         for (uint i = 0u; i < u_nLightsDirectional; i++) {
