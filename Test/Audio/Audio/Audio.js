054db2c6c901f0aa1f0ed1b20170f2368ca160bd
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-01-29 17:38:09 +0100

    start test toggle

--- Test/Audio/Audio/Audio.js
+++ Test/Audio/Audio/Audio.js
@@ -4,12 +4,22 @@ var AudioTest;
 (function (AudioTest) {
     var ƒ = FudgeCore;
     window.addEventListener("click", start);
-    async function start() {
+    window.addEventListener("keydown", toggle);
+    let cmpAudio;
+    let toggled = false;
+    async function start(_event) {
         let audio = await ƒ.Audio.load("mario_piano.mp3");
         ƒ.Debug.log(audio);
-        let cmpAudio = new ƒ.ComponentAudio(audio);
+        cmpAudio = new ƒ.ComponentAudio(audio);
         ƒ.Debug.log(cmpAudio);
         cmpAudio.play(true);
+    }
+    function toggle(_event) {
+        ƒ.Debug.log("Toggle");
+        if (toggled)
+            cmpAudio.dispatchEvent(new Event("componentRemove" /* COMPONENT_REMOVE */));
+        else
+            cmpAudio.dispatchEvent(new Event("componentAdd" /* COMPONENT_ADD */));
     }
 })(AudioTest || (AudioTest = {}));
 //# sourceMappingURL=Audio.js.map
--- Test/Audio/Audio/Audio.ts
+++ Test/Audio/Audio/Audio.ts
@@ -2,14 +2,25 @@
 namespace AudioTest {
   import ƒ = FudgeCore;
   window.addEventListener("click", start);
+  window.addEventListener("keydown", toggle)
+  let cmpAudio: ƒ.ComponentAudio;
+  let toggled: boolean = false;
 
-  async function start(): Promise<void> {
+  async function start(_event: Event): Promise<void> {
     let audio: ƒ.Audio = await ƒ.Audio.load("mario_piano.mp3");
     ƒ.Debug.log(audio);
 
-    let cmpAudio: ƒ.ComponentAudio = new ƒ.ComponentAudio(audio);
+    cmpAudio = new ƒ.ComponentAudio(audio);
     ƒ.Debug.log(cmpAudio);
 
     cmpAudio.play(true);
+  }
+
+  function toggle(_event: Event): void {
+    ƒ.Debug.log("Toggle");
+    if (toggled)
+      cmpAudio.dispatchEvent(new Event(ƒ.EVENT.COMPONENT_REMOVE));
+    else
+      cmpAudio.dispatchEvent(new Event(ƒ.EVENT.COMPONENT_ADD));
   }
 }
