24213e5c74a39f24d4011b397b7f274c19669591
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-02-06 19:57:35 +0100

    lookAt cleanup

--- Test/Shading/LightMoving/LightMoving.ts
+++ Test/Shading/LightMoving/LightMoving.ts
@@ -18,15 +18,15 @@ namespace TextureTest {
         lights.addComponent(cmpLightAmbient);
 
         let cmpLightDirectionalRed: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightDirectional(ƒ.Color.CSS("RED")));
-        cmpLightDirectionalRed.pivot.lookAt(new ƒ.Vector3(-1, 1, 0));
+        cmpLightDirectionalRed.pivot.lookAt(new ƒ.Vector3(-1, -1, 0));
         lights.addComponent(cmpLightDirectionalRed);
 
         let cmpLightDirectionalGreen: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightDirectional(ƒ.Color.CSS("GREEN")));
-        cmpLightDirectionalGreen.pivot.lookAt(new ƒ.Vector3(0, 1, -1));
+        cmpLightDirectionalGreen.pivot.lookAt(new ƒ.Vector3(0, -1, -1));
         lights.addComponent(cmpLightDirectionalGreen);
 
         let cmpLightDirectionalBlue: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightDirectional( ƒ.Color.CSS("BLUE")));
-        cmpLightDirectionalBlue.pivot.lookAt(new ƒ.Vector3(0.5, 1, 0.5));
+        cmpLightDirectionalBlue.pivot.lookAt(new ƒ.Vector3(0.5, -1, 0.5));
         lights.addComponent(cmpLightDirectionalBlue);
 
         let branch: ƒ.Node = new ƒ.Node("Branch");
