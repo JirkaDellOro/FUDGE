5e57696437441ab1cfa9d29dfb7f01ac553ea56c
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-07-23 20:25:30 +0200

    dynamically adding panels

--- Miscellaneous/Experiments/Jirka/GoldenLayout/GoldenLayoutTest.js
+++ Miscellaneous/Experiments/Jirka/GoldenLayout/GoldenLayoutTest.js
@@ -3,66 +3,87 @@ var GoldenLayoutTest;
 (function (GoldenLayoutTest) {
     let glDoc;
     window.addEventListener("load", start);
-    function start() {
-        let configPanel = {
-            content: [{
-                    type: "row",
-                    content: [
-                        {
-                            type: "component",
-                            componentName: "panel",
-                            componentState: { text: "Panel 1" }
-                        }, {
-                            type: "component",
-                            componentName: "panel",
-                            componentState: { text: "Panel 2" }
-                        }
-                    ]
-                }]
-        };
-        let Panel = /** @class */ (() => {
-            class Panel {
-                constructor(_container, _state) {
-                    let div = document.createElement("div");
-                    div.style.height = "100%";
-                    div.style.width = "100%";
-                    _container.getElement().append(div);
-                    let gl = new GoldenLayout(Panel.config, div);
-                    gl.registerComponent("example", example);
-                    gl.on("stateChanged", () => gl.updateSize());
-                    gl.init();
-                }
-            }
-            Panel.config = {
-                type: "row",
+    class Panel extends GoldenLayout {
+        constructor(_container = null, _state) {
+            // let gl: GoldenLayout = new GoldenLayout(Panel.config, div);
+            let div = document.createElement("div");
+            super(Panel.config, div);
+            // let div: HTMLDivElement = document.createElement("div");
+            // console.log(div, this.container);
+            console.log(_container);
+            div.style.height = "100%";
+            div.style.width = "100%";
+            if (_container)
+                _container.getElement().append(div);
+            this.registerComponent("example", example);
+            this.on("stateChanged", () => this.updateSize());
+            this.init();
+        }
+        static add() {
+            let config = {
+                type: "stack",
                 content: [{
-                        type: "stack",
-                        content: [{
-                                type: "component",
-                                componentName: "example",
-                                componentState: { text: "1" }
-                            }, {
-                                type: "component",
-                                componentName: "example",
-                                componentState: { text: "2" }
-                            }, {
-                                type: "component",
-                                componentName: "example",
-                                componentState: { text: "3" }
-                            }]
+                        type: "component",
+                        componentName: "panel",
+                        componentState: { text: "Panel 3" },
+                        title: "Panel3"
                     }]
             };
-            return Panel;
-        })();
-        function example(_container, _state) {
-            let div = document.createElement("div");
-            div.style.backgroundColor = "red";
-            div.innerHTML = `<h2>${_state.text}</h2>`;
-            _container.getElement().html(div);
+            glDoc.root.contentItems[0].addChild(config);
         }
+    }
+    Panel.config = {
+        type: "row",
+        content: [{
+                type: "stack",
+                content: [{
+                        type: "component",
+                        componentName: "example",
+                        componentState: { text: "1" },
+                        title: "Example1"
+                    }, {
+                        type: "component",
+                        componentName: "example",
+                        componentState: { text: "2" },
+                        title: "Example2"
+                    }, {
+                        type: "component",
+                        componentName: "example",
+                        componentState: { text: "3" },
+                        title: "Example3"
+                    }]
+            }]
+    };
+    let configPanel = {
+        content: [{
+                type: "row",
+                content: [
+                    {
+                        type: "component",
+                        componentName: "panel",
+                        componentState: { text: "Panel 1" },
+                        title: "Panel1"
+                    }, {
+                        type: "component",
+                        componentName: "panel",
+                        componentState: { text: "Panel 2" },
+                        title: "Panel2"
+                    }
+                ]
+            }]
+    };
+    function start() {
+        let btnAdd = document.querySelector("button");
+        btnAdd.addEventListener("click", Panel.add);
         glDoc = new GoldenLayout(configPanel);
         glDoc.registerComponent("panel", Panel);
         glDoc.init();
+    }
+    function example(_container, _state) {
+        let div = document.createElement("div");
+        div.style.backgroundColor = "red";
+        div.innerHTML = `<h2>${_state.text}</h2>`;
+        _container.getElement().html(div);
     }
 })(GoldenLayoutTest || (GoldenLayoutTest = {}));
 //# sourceMappingURL=GoldenLayoutTest.js.map
--- Miscellaneous/Experiments/Jirka/GoldenLayout/GoldenLayoutTest.ts
+++ Miscellaneous/Experiments/Jirka/GoldenLayout/GoldenLayoutTest.ts
@@ -2,67 +2,93 @@ namespace GoldenLayoutTest {
   let glDoc: GoldenLayout;
   window.addEventListener("load", start);
 
-  function start(): void {
-    let configPanel: GoldenLayout.Config = {
+  class Panel extends GoldenLayout {
+    public static config: GoldenLayout.ItemConfig = {
+      type: "row",
       content: [{
-        type: "row",
-        content: [
-          {
-            type: "component",
-            componentName: "panel",
-            componentState: { text: "Panel 1" }
-          }, {
-            type: "component",
-            componentName: "panel",
-            componentState: { text: "Panel 2" }
-          }
-        ]
+        type: "stack",
+        content: [{
+          type: "component",
+          componentName: "example",
+          componentState: { text: "1" },
+          title: "Example1"
+        }, {
+          type: "component",
+          componentName: "example",
+          componentState: { text: "2" },
+          title: "Example2"
+        }, {
+          type: "component",
+          componentName: "example",
+          componentState: { text: "3" },
+          title: "Example3"
+        }]
       }]
     };
 
-    class Panel {
-      public static config: GoldenLayout.ItemConfig = {
-        type: "row",
+    constructor(_container: GoldenLayout.Container = null, _state: { text: string }) {
+      // let gl: GoldenLayout = new GoldenLayout(Panel.config, div);
+      let div: HTMLDivElement = document.createElement("div");
+      super(Panel.config, div);
+      // let div: HTMLDivElement = document.createElement("div");
+      // console.log(div, this.container);
+      console.log(_container);
+      div.style.height = "100%";
+      div.style.width = "100%";
+      if (_container)
+        _container.getElement().append(div);
+      this.registerComponent("example", example);
+
+      this.on("stateChanged", () => this.updateSize());
+      this.init();
+    }
+
+    public static add(): void {
+      let config: GoldenLayout.ItemConfig = {
+        type: "stack",
         content: [{
-          type: "stack",
-          content: [{
-            type: "component",
-            componentName: "example",
-            componentState: { text: "1" }
-          }, {
-            type: "component",
-            componentName: "example",
-            componentState: { text: "2" }
-          }, {
-            type: "component",
-            componentName: "example",
-            componentState: { text: "3" }
-          }]
+          type: "component",
+          componentName: "panel",
+          componentState: { text: "Panel 3" },
+          title: "Panel3"
         }]
       };
-
-      constructor(_container: GoldenLayout.Container, _state: { text: string }) {
-        let div: HTMLDivElement = document.createElement("div");
-        div.style.height = "100%";
-        div.style.width = "100%";
-        _container.getElement().append(div);
-        let gl: GoldenLayout = new GoldenLayout(Panel.config, div);
-        gl.registerComponent("example", example);
-
-        gl.on("stateChanged", () => gl.updateSize());
-        gl.init();
-      }
+      glDoc.root.contentItems[0].addChild(config);
     }
+  }
 
-    function example(_container: GoldenLayout.Container, _state: { text: string }): void {
-      let div: HTMLDivElement = document.createElement("div");
-      div.style.backgroundColor = "red";
-      div.innerHTML = `<h2>${_state.text}</h2>`;
-      _container.getElement().html(div);
-    }
+  let configPanel: GoldenLayout.Config = {
+    content: [{
+      type: "row",
+      content: [
+        {
+          type: "component",
+          componentName: "panel",
+          componentState: { text: "Panel 1" },
+          title: "Panel1"
+        }, {
+          type: "component",
+          componentName: "panel",
+          componentState: { text: "Panel 2" },
+          title: "Panel2"
+        }
+      ]
+    }]
+  };
+
+  function start(): void {
+    let btnAdd: HTMLButtonElement = document.querySelector("button");
+    btnAdd.addEventListener("click", Panel.add);
 
     glDoc = new GoldenLayout(configPanel);
     glDoc.registerComponent("panel", Panel);
     glDoc.init();
+  }
+
+  function example(_container: GoldenLayout.Container, _state: { text: string }): void {
+    let div: HTMLDivElement = document.createElement("div");
+    div.style.backgroundColor = "red";
+    div.innerHTML = `<h2>${_state.text}</h2>`;
+    _container.getElement().html(div);
   }
 }
