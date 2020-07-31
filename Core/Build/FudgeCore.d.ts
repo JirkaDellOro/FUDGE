1b3ead4e77911df59e415aabee463c2d6e2c4372
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-07-31 13:44:37 +0200

    ProjectStructure.svg

--- Core/Build/FudgeCore.d.ts
+++ Core/Build/FudgeCore.d.ts
@@ -1021,7 +1021,7 @@ declare namespace FudgeCore {
         /**
          * Determines FUDGE-graph to listen to. Each [[ComponentAudio]] in the graph will connect to this contexts master gain, all others disconnect.
          */
-        listenTo: (_graph: Node) => void;
+        listenTo: (_graph: Node | null) => void;
         /**
          * Retrieve the FUDGE-graph currently listening to
          */
@@ -1029,7 +1029,7 @@ declare namespace FudgeCore {
         /**
          * Set the [[ComponentAudioListener]] that serves the spatial location and orientation for this contexts listener
          */
-        listen: (_cmpListener: ComponentAudioListener) => void;
+        listen: (_cmpListener: ComponentAudioListener | null) => void;
         /**
          * Updates the spatial settings of the AudioNodes effected in the current FUDGE-graph
          */
