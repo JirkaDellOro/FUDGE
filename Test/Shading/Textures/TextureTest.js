889ad8ef2ec7034ba553aaf3db26212c2a82cbc7
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-02-07 15:02:32 +0100

    Improved Texture Test and Pyramid UVs

--- Test/Shading/Textures/TextureTest.js
+++ Test/Shading/Textures/TextureTest.js
@@ -1,19 +1,16 @@
 var TextureTest;
 (function (TextureTest) {
     var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
     ƒ.Render.initialize(true, true);
     window.addEventListener("load", init);
     function init(_event) {
         let coatTextured = new ƒ.CoatTextured();
         let material = new ƒ.Material("Textured", ƒ.ShaderTexture, coatTextured);
-        let quad = Scenes.createCompleteMeshNode("Quad", material, new ƒ.MeshQuad());
-        let cube = Scenes.createCompleteMeshNode("Cube", material, new ƒ.MeshCube());
-        let pyramid = Scenes.createCompleteMeshNode("Pyramid", material, new ƒ.MeshPyramid());
-        cube.mtxLocal.translateX(0.7);
-        // cube.cmpTransform.rotateX(-45);
-        cube.mtxLocal.rotateY(-45);
-        pyramid.mtxLocal.translateX(-0.7);
-        pyramid.mtxLocal.rotateY(180);
+        coatTextured.texture.mipmap = ƒ.MIPMAP.BLURRY;
+        let quad = new ƒAid.Node("Quad", ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.ZERO()), material, new ƒ.MeshQuad());
+        let cube = new ƒAid.Node("Quad", ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(-0.7)), material, new ƒ.MeshCube());
+        let pyramid = new ƒAid.Node("Quad", ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(0.7)), material, new ƒ.MeshPyramid());
         let graph = new ƒ.Node("Graph");
         graph.addChild(quad);
         graph.addChild(cube);
@@ -21,13 +18,14 @@ var TextureTest;
         let viewport = new ƒ.Viewport();
         let cmpCamera = Scenes.createCamera(new ƒ.Vector3(0, 2, 3), new ƒ.Vector3(0, 0, 0));
         viewport.initialize("Viewport", graph, cmpCamera, document.querySelector("canvas"));
-        viewport.draw();
-        window.setInterval(function () {
-            pyramid.mtxLocal.rotateX(1);
-            cube.mtxLocal.rotateY(-1);
-            quad.mtxLocal.rotateZ(1);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, function () {
+            let speed = 0.5;
+            pyramid.mtxLocal.rotateX(speed);
+            cube.mtxLocal.rotateY(speed);
+            quad.mtxLocal.rotateZ(speed);
             viewport.draw();
-        }, 20);
+        });
+        ƒ.Loop.start();
     }
 })(TextureTest || (TextureTest = {}));
 //# sourceMappingURL=TextureTest.js.map
