830a66c318b463078516c14d94c802205645bfc0
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-03-01 13:19:27 +0100

    delete faces with identical vectors, further simplfied

--- Core/Source/Mesh/MeshExtrusion.ts
+++ Core/Source/Mesh/MeshExtrusion.ts
@@ -51,76 +51,77 @@ namespace FudgeCore {
 
     private extrude(_transforms: Matrix4x4[] = MeshExtrusion.transformsDefault): void {
       this.ƒtransforms = _transforms;
+
+      // save original polygon
       let polygon: Vector3[] = [];
       for (let i: number = 0; i < this.vertices.length; i += 3)
         polygon.push(new Vector3(this.vertices[i], this.vertices[i + 1], this.vertices[i + 2]));
 
       let nTransforms: number = _transforms.length;
-      let lengthVertexArrayPolygon: number = this.vertices.length;
       let nVerticesPolygon: number = polygon.length;
-      let nVerticesExtrusion: number = 2 * nVerticesPolygon + (nVerticesPolygon + 1) * nTransforms; // base + lid + wrapper + closing points
-      let lengthVertexArrayExtrusion: number = nVerticesExtrusion * 3;
 
       let nFacesPolygon: number = nVerticesPolygon - 2;
       let nIndicesPolygon: number = nFacesPolygon * 3;
-      let nFacesExtrusion: number = nFacesPolygon * 2 + nVerticesPolygon * 2 * (nTransforms - 1);
-      let nIndicesExtrusion: number = nFacesExtrusion * 3;
 
-      let vertices: Float32Array = new Float32Array(lengthVertexArrayExtrusion);
-
+      let vertices: Vector3[] = [];
       // create base by transformation of polygon with first transform
       let base: Vector3[] = polygon.map((_v: Vector3) => Vector3.TRANSFORMATION(_v, _transforms[0], true));
-      vertices.set(base.map((_v: Vector3) => [_v.x, _v.y, _v.z]).flat());
+      vertices.push(...base);
       // create lid by transformation of polygon with last transform
       let lid: Vector3[] = polygon.map((_v: Vector3) => Vector3.TRANSFORMATION(_v, _transforms[nTransforms - 1], true));
-      vertices.set(lid.map((_v: Vector3) => [_v.x, _v.y, _v.z]).flat(), lengthVertexArrayPolygon);
+      vertices.push(...lid);
 
       // duplicate first vertex of polygon to the end to create a texturable wrapping
       polygon.push(polygon[0].copy);
-      let index: number = lengthVertexArrayPolygon * 2;
       for (let transform of _transforms) {
         let wrap: Vector3[] = polygon.map((_v: Vector3) => Vector3.TRANSFORMATION(_v, transform, true));
-        vertices.set(wrap.map((_v: Vector3) => [_v.x, _v.y, _v.z]).flat(), index);
-        index += polygon.length * 3;
+        vertices.push(...wrap);
       }
 
-      this.ƒvertices = vertices;
 
       // copy indices to new index array
-      let indices: Uint16Array = new Uint16Array(nIndicesExtrusion);
-      indices.set(this.indices, 0);
+      let indices: number[] = [];
+      indices.push(...this.indices);
 
       // copy indices for second polygon and reverse sequence
       for (let i: number = 0; i < nIndicesPolygon; i += 3) {
-        indices[nIndicesPolygon + i] = this.indices[i] + nVerticesPolygon;
-        indices[nIndicesPolygon + i + 1] = this.indices[i + 2] + nVerticesPolygon;
-        indices[nIndicesPolygon + i + 2] = this.indices[i + 1] + nVerticesPolygon;
+        indices.push(this.indices[i] + nVerticesPolygon);
+        indices.push(this.indices[i + 2] + nVerticesPolygon);
+        indices.push(this.indices[i + 1] + nVerticesPolygon);
       }
 
       // create indizes for wrapper
-      index = nIndicesPolygon * 2;
       for (let t: number = 0; t < nTransforms - 1; t++)
         for (let i: number = 0; i < nVerticesPolygon; i++) {
           let vertex: number = i + (2 + t) * nVerticesPolygon + t;
-          indices[index++] = vertex;
-          indices[index++] = vertex + nVerticesPolygon + 1;
-          indices[index++] = vertex + nVerticesPolygon + 2;
-          indices[index++] = vertex;
-          indices[index++] = vertex + nVerticesPolygon + 2;
-          indices[index++] = vertex + 1;
+          indices.push(vertex);
+          indices.push(vertex + nVerticesPolygon + 1);
+          indices.push(vertex + nVerticesPolygon + 2);
+          indices.push(vertex);
+          indices.push(vertex + nVerticesPolygon + 2);
+          indices.push(vertex + 1);
         }
-      this.ƒindices = indices;
+
+      //delete "non"-faces with two identical vectors
+      for (let i: number = indices.length - 3; i >= 0; i -= 3) {
+        let v0: Vector3 = vertices[indices[i]];
+        let v1: Vector3 = vertices[indices[i + 1]];
+        let v2: Vector3 = vertices[indices[i + 2]];
+        if (v0.equals(v1) || v2.equals(v1) || v0.equals(v2))
+          indices.splice(i, 3);
+      }
+
 
       let nTextureUVs: number = this.textureUVs.length;
-      let textureUVs: Float32Array = new Float32Array(nVerticesExtrusion * 2);
-      textureUVs.set(this.textureUVs, 0);
-      textureUVs.set(this.textureUVs, nTextureUVs);
+      let textureUVs: number[] = [];
+      textureUVs.push(...this.textureUVs);
+      textureUVs.push(...this.textureUVs);
 
       // TODO: wrap texture nicer respecting the distances between vertices, see lengths polygon etc.
       // let sumLengths: number = lengthsPolygon.reduce((_sum, _value) => _sum + _value);
+      let index: number = nTextureUVs * 2;
       let incV: number = 1 / nVerticesPolygon;
       let incU: number = 1 / (nTransforms - 1);
-      index = nTextureUVs * 2;
       let u: number = 1;
       for (let t: number = 0; t < nTransforms - 1; t++) {
         let v: number = 0;
@@ -136,7 +137,9 @@ namespace FudgeCore {
         u -= incU;
       }
 
-      this.ƒtextureUVs = textureUVs;
+      this.ƒvertices = new Float32Array(vertices.map((_v: Vector3) => [_v.x, _v.y, _v.z]).flat());
+      this.ƒindices = new Uint16Array(indices);
+      this.ƒtextureUVs = new Float32Array(textureUVs);
     }
 
     // private calculatePolygonLengths(): number[] {
