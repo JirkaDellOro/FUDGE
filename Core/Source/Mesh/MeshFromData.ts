ce5f304ad3cd0ef229fc2e2b43530453d685904c
Simon Storl-Schulke <simonstoschu@googlemail.com>
2021-04-25 09:38:36 +0200

    Update MeshFromData.ts

--- Core/Source/Mesh/MeshFromData.ts
+++ Core/Source/Mesh/MeshFromData.ts
@@ -2,33 +2,33 @@ namespace FudgeCore {
 
     /** Allows to create custom meshes from given Data */
     export class MeshFromData extends Mesh {
-            protected vertices: Float32Array;
-            protected textureUVs: Float32Array;
-            protected indices: Uint16Array;
-            protected faceNormals: Float32Array;
+            protected verticesToSet: Float32Array;
+            protected textureUVsToSet: Float32Array;
+            protected indicesToSet: Uint16Array;
+            protected faceNormalsToSet: Float32Array;
         
         public constructor(_vertices: Float32Array, _textureUVs: Float32Array, _indices: Uint16Array, _faceNormals: Float32Array) {
             super();
-            this.vertices = _vertices;
-            this.textureUVs = _textureUVs;
-            this.indices = _indices;
-            this.faceNormals = _faceNormals
+            this.verticesToSet = _vertices;
+            this.textureUVsToSet = _textureUVs;
+            this.indicesToSet = _indices;
+            this.faceNormalsToSet = _faceNormals
         }
 
         protected createVertices(): Float32Array {
-            return this.vertices;
+            return this.verticesToSet;
         }
 
         protected createTextureUVs(): Float32Array {
-            return this.textureUVs;
+            return this.textureUVsToSet;
         }
 
         protected createIndices(): Uint16Array {
-            return this.indices;
+            return this.indicesToSet;
         }
 
         protected createFaceNormals(): Float32Array {
-            return this.faceNormals;
+            return this.faceNormalsToSet;
         }
     }
 }
