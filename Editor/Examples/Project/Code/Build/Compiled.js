693bc432bb5c28d74e8114ba87104740c89543e3
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-06-10 17:42:43 +0200

    timer as private field

--- Editor/Examples/Project/Code/Build/Compiled.js
+++ Editor/Examples/Project/Code/Build/Compiled.js
@@ -58,15 +58,16 @@ var Script;
             super();
             // public static readonly iSubclass: number = ƒ.Component.registerSubclass(TimerMessage);
             this.prefix = "Script: ";
-            this.count = 0;
+            this.test = false;
+            this.#count = 0;
             this.hndTimer = (_event) => {
-                console.log(this.prefix + this.count++);
+                console.log(this.prefix + this.#count++);
             };
             this.hndAddComponent = (_event) => {
-                this.timer = new ƒ.Timer(ƒ.Time.game, 1000, 0, this.hndTimer);
+                this.#timer = new ƒ.Timer(ƒ.Time.game, 1000, 0, this.hndTimer);
             };
             this.hndRemoveComponent = (_event) => {
-                this.timer.clear();
+                this.#timer.clear();
                 this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndAddComponent);
                 this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndRemoveComponent);
             };
@@ -75,6 +76,8 @@ var Script;
             this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndAddComponent);
             this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndRemoveComponent);
         }
+        #count;
+        #timer;
     }
     Script.TimerMessage = TimerMessage;
 })(Script || (Script = {}));
