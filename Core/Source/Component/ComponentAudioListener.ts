329e9bef905be29487318fae30ed1212ece2b86b
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-08-28 13:35:29 +0200

    Workaround for Firefox using deprecated methods

--- Core/Source/Component/ComponentAudioListener.ts
+++ Core/Source/Component/ComponentAudioListener.ts
@@ -21,17 +21,23 @@ namespace FudgeCore {
       let forward: Vector3 = Vector3.TRANSFORMATION(Vector3.Z(1), mtxResult, false);
       let up: Vector3 = Vector3.TRANSFORMATION(Vector3.Y(), mtxResult, false);
 
-      _listener.positionX.value = position.x;
-      _listener.positionY.value = position.y;
-      _listener.positionZ.value = position.z;
+      if (_listener.positionX != undefined) {
+        _listener.positionX.value = position.x;
+        _listener.positionY.value = position.y;
+        _listener.positionZ.value = position.z;
 
-      _listener.forwardX.value = forward.x;
-      _listener.forwardY.value = forward.y;
-      _listener.forwardZ.value = forward.z;
+        _listener.forwardX.value = forward.x;
+        _listener.forwardY.value = forward.y;
+        _listener.forwardZ.value = forward.z;
 
-      _listener.upX.value = up.x;
-      _listener.upY.value = up.y;
-      _listener.upZ.value = up.z;
+        _listener.upX.value = up.x;
+        _listener.upY.value = up.y;
+        _listener.upZ.value = up.z;
+      } else {
+        // Firefox still uses deprecated methods. TODO: delete as soon as possible!
+        _listener.setPosition(position.x, position.y, position.z);
+        _listener.setOrientation(forward.x, forward.y, forward.z, up.x, up.y, up.z);
+      }
 
       // Debug.log(mtxResult.translation.toString(), forward.toString(), up.toString());
     }
