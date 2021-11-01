dc8bc469e6b92eefe3e96d68820fb219fdcc8206
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-11-01 07:38:17 +0100

    separated login and peer-connect

--- Test/Network/Client/Client.js
+++ Test/Network/Client/Client.js
@@ -6,23 +6,28 @@ var ClientWebSocket;
         console.log(settings);
         let domServer = document.forms[0].querySelector("input[name=server");
         let domLogin = document.forms[0].querySelector("input[name=login");
+        document.forms[1].querySelector("input#partner").setAttribute("value", settings.partner);
         domServer.setAttribute("value", settings.server);
         domLogin.setAttribute("value", settings.login);
-        document.forms[1].querySelector("button#connect").addEventListener("click", connect);
+        document.forms[1].querySelector("button#login").addEventListener("click", login);
+        document.forms[1].querySelector("button#peer").addEventListener("click", connectToPeer);
         document.forms[2].querySelector("button#sendTCP").addEventListener("click", sendTCP);
         document.forms[2].querySelector("button#sendRTC").addEventListener("click", sendRTC);
         let client;
         const messageTypes = FudgeNetwork.MESSAGE_TYPE;
-        async function connect() {
+        async function login() {
             let formData = new FormData(document.forms[1]);
             let connectionType = formData.get("type")?.toString();
             console.log(`%c${connectionType}`, "color:blue; font-size: large");
             switch (connectionType) {
                 case "WebSocket":
                     client = new FudgeNetwork.ClientManagerWebSocketOnly();
+                    document.forms[2].querySelector("button#sendTCP").removeAttribute("disabled");
                     break;
                 case "SinglePeer":
                     client = new FudgeNetwork.ClientManagerSinglePeer();
+                    document.forms[1].querySelector("button#peer").removeAttribute("disabled");
+                    document.forms[2].querySelector("button#sendRTC").removeAttribute("disabled");
                     break;
                 default:
                     console.error("Select a connection type!");
@@ -31,16 +36,14 @@ var ClientWebSocket;
             console.log("Create", client);
             await client.connectToSignalingServer(domServer.value);
             await delay(1000); // replace with a signal indicating connection
-            console.log("Connected");
+            console.log("Logged in");
             client.checkChosenUsernameAndCreateLoginRequest(domLogin.value);
             console.log("Username checked", domLogin.value);
-            // let messageToSend: ƒ.General = new client.NetworkMessageMessageToServer(client.getLocalClientId(), "Hallo", client.localUserName);
-            // client.sendTextMessageToSignalingServer(messageToSend);
-            if (connectionType == "SinglePeer") {
-                let domPartner = document.forms[1].querySelector("input#partner");
-                client.checkUsernameToConnectToAndInitiateConnection(domPartner.value);
-                client.ownPeerConnection.addEventListener("receive", receiveRTC);
-            }
+        }
+        async function connectToPeer(_event) {
+            let domPartner = document.forms[1].querySelector("input#partner");
+            client.checkUsernameToConnectToAndInitiateConnection(domPartner.value);
+            client.ownPeerConnection.addEventListener("receive", receiveRTC);
         }
         async function sendTCP() {
             let domMessage = document.forms[2].querySelector("input#messageTCP");
@@ -55,7 +58,7 @@ var ClientWebSocket;
         }
         async function receiveRTC(_event) {
             let domReceive = document.forms[2].querySelector("textarea#receivedRTC");
-            domReceive.value += _event.detail.originatorUserName + ": " + _event.detail.messageData;
+            domReceive.value += _event.detail.originatorUserName + ": " + _event.detail.messageData + "\n";
         }
         function delay(_milisec) {
             return new Promise(resolve => {
