e6c6e6c877c092bef26d766a5a463cd8eef5af7b
Matthias Roming <matze.ro99@gmail.com>
2021-12-02 07:51:20 +0100

    added skin flat shader

---
+++ Core/Source/Shader/ShaderFlatSkin.ts
@@ -0,0 +1,73 @@
+namespace FudgeCore {
+  /**
+   * Single color flat skin deformation shading
+   * @authors Matthias Roming, HFU, 2021
+   */
+  // @RenderInjectorShader.decorate
+  export abstract class ShaderFlatSkin extends Shader {
+    public static readonly iSubclass: number = Shader.registerSubclass(ShaderFlatSkin);
+
+    public static getCoat(): typeof Coat {
+      return ShaderFlat.getCoat();
+    }
+
+    public static getVertexShaderSource(): string {
+      return `#version 300 es
+
+      in vec3 a_position;
+      in vec3 a_normal;
+      
+      flat out vec4 v_color;
+      
+      uniform mat4 u_world;
+      uniform mat4 u_projection;
+      
+      // Bones
+      const uint MAX_BONES = 256u;
+      
+      in vec4 a_iBone;
+      in vec4 a_weight;
+
+      uniform mat4 u_mtxBones[MAX_BONES];
+      
+      // Lights
+      struct LightAmbient {
+        vec4 color;
+      };
+      
+      struct LightDirectional {
+        vec4 color;
+        vec3 direction;
+      };
+      
+      const uint MAX_LIGHTS_DIRECTIONAL = 10u;
+      
+      uniform LightAmbient u_ambient;
+      uniform uint u_nLightsDirectional;
+      uniform LightDirectional u_directional[MAX_LIGHTS_DIRECTIONAL];
+      
+      void main() {  
+        mat4 skinMatrix =
+          a_weight.x * u_mtxBones[uint(a_iBone.x)] +
+          a_weight.y * u_mtxBones[uint(a_iBone.y)] +
+          a_weight.z * u_mtxBones[uint(a_iBone.z)] +
+          a_weight.w * u_mtxBones[uint(a_iBone.w)];
+      
+        gl_Position = u_projection * skinMatrix * vec4(a_position, 1.0);
+        vec3 normal = normalize(transpose(inverse(mat3(u_world * skinMatrix))) * a_normal);
+        
+        v_color = u_ambient.color;
+        for (uint i = 0u; i < u_nLightsDirectional; i++) {
+          float illumination = -dot(normal, u_directional[i].direction);
+          if (illumination > 0.0f)
+            v_color += illumination * u_directional[i].color;
+        }
+        v_color.a = 1.0;
+      }`;
+    }
+
+    public static getFragmentShaderSource(): string {
+      return ShaderFlat.getFragmentShaderSource();
+    }
+  }
+}
