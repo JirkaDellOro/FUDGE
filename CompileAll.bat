76f8adb82e21878670b9a8547ac3c652c03a7e85
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2022-03-30 10:42:51 +0200

    Full recompile and documentation

--- CompileAll.bat
+++ CompileAll.bat
@@ -1,7 +1,6 @@
-goto Net
+::goto Test
 
-@rem Core
-@rem CompileShaders
+:Core
 echo --- Compile Shaders
 cd Core/Source/Shader/
 call node CompileShaders.mjs CompileShaders.json
@@ -14,9 +13,8 @@ echo --- Generate Documentation
 call typedoc
 cd ..
 cd ..
-pause
 
-@rem Aid
+:Aid
 echo --- Compile Aid
 cd Aid/Source/
 call tsc -p tsconfig.json
@@ -24,9 +22,8 @@ echo --- Generate Documentation
 call typedoc
 cd ..
 cd ..
-pause
 
-@rem UserInterface
+:UserInterface
 echo --- Compile UserInterface
 cd UserInterface/Source/
 call tsc -p tsconfig.json
@@ -34,10 +31,8 @@ echo --- Generate Documentation
 call typedoc
 cd ..
 cd ..
-pause
 
 :Net
-@rem Net
 echo --- Compile Net
 cd Net
 call tsc -p ./tsconfig.message.json
@@ -45,5 +40,11 @@ echo module.exports = {FudgeNet: FudgeNet}; >> .\\Source\\Server\\Message.js
 call AddExport .\\Source\\Server\\Message.d.ts
 call tsc -p ./tsconfig.server.json
 call tsc -p ./tsconfig.client.json
+cd ..
+
+:Test
+echo --- Compile Test
+cd Test
+call tsc -p tsconfig.json
 cd ..
 pause
