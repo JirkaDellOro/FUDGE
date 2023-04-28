9cf11405c7a3ac849ef74e338a1a04d1d259cf17
JP <jonas.plotzky@hs-furtwangen.de>
2023-04-28 11:11:47 +0200

    add orbit camera to test scene

--- Test/FBX/FBXLoaderTest.js
+++ Test/FBX/FBXLoaderTest.js
@@ -1,29 +1,34 @@
-///<reference path="./../../Core/Build/FudgeCore.d.ts"/>
+///<reference path="../../Core/Build/FudgeCore.d.ts"/>
+///<reference path="../../Aid/Build/FudgeAid.d.ts"/>
 var SkeletonTest;
-///<reference path="./../../Core/Build/FudgeCore.d.ts"/>
+///<reference path="../../Core/Build/FudgeCore.d.ts"/>
+///<reference path="../../Aid/Build/FudgeAid.d.ts"/>
 (function (SkeletonTest) {
     var ƒ = FudgeCore;
-    const mouse = {
-        position: new ƒ.Vector2()
-    };
+    var ƒAid = FudgeAid;
+    let camera;
+    let speedCameraRotation = 0.2;
+    let speedCameraTranslation = 0.1;
+    let cntMouseX = new ƒ.Control("MouseX", speedCameraRotation);
+    let cntMouseY = new ƒ.Control("MouseY", speedCameraRotation);
     window.addEventListener("load", init);
-    let timeSpan;
     async function init() {
-        const canvas = document.querySelector("canvas");
-        timeSpan = document.querySelector("span");
         // const loader: ƒ.FBXLoader = await ƒ.FBXLoader.LOAD("./TriangularPrism.fbx");
         // const loader: ƒ.FBXLoader = await ƒ.FBXLoader.LOAD("./animated_arm.fbx");
         const loader = await ƒ.FBXLoader.LOAD("./Unarmed Walk Forward.fbx");
-        // track mouse position relative to canvas center
-        window.addEventListener("mousemove", (_event) => {
-            mouse.position.x = _event.clientX - canvas.width / 2;
-            mouse.position.y = _event.clientY - canvas.height / 2;
-        });
         // test loading a mesh
-        console.log(await loader.getMesh(0));
+        // console.log(await loader.getMesh(0));
         // load scene
-        const scene = await loader.getScene(0);
-        console.log(scene);
+        const graph = await loader.getScene(0);
+        console.log(graph);
+        // camera setup
+        const cmpCamera = new ƒ.ComponentCamera();
+        camera = new ƒAid.CameraOrbit(cmpCamera, 500, 80, 2, 1000);
+        camera.axisRotateX.addControl(cntMouseY);
+        camera.axisRotateY.addControl(cntMouseX);
+        cmpCamera.clrBackground.setHex("4472C4FF");
+        graph.addChild(camera);
+        camera.mtxLocal.translateY(100);
         // let skeleton: ƒ.Node = scene;
         // for (const node of scene)
         //   if (node != scene && node.name == "Skeleton0")
@@ -57,89 +62,64 @@ var SkeletonTest;
         // loader.fbx.objects.all.forEach(_object => _object.load());
         // console.log(loader.nodes);
         // console.log(loader.fbx);
-        // setup camera
-        const rotatorX = new ƒ.Node("RotatorX");
-        rotatorX.addComponent(new ƒ.ComponentTransform());
-        scene.addChild(rotatorX);
-        const rotatorY = new ƒ.Node("RotatorY");
-        rotatorY.addComponent(new ƒ.ComponentTransform());
-        rotatorX.addChild(rotatorY);
-        const camera = new ƒ.Node("Camera");
-        camera.addComponent(new ƒ.ComponentCamera());
-        camera.addComponent(new ƒ.ComponentTransform());
-        camera.getComponent(ƒ.ComponentCamera).clrBackground.setHex("4472C4FF");
-        rotatorX.mtxLocal.translateY(80); // 80
-        camera.mtxLocal.translateZ(300); // 30
-        camera.mtxLocal.lookAt(ƒ.Vector3.Y(camera.mtxLocal.translation.y), camera.mtxLocal.getY());
-        rotatorY.addChild(camera);
         // setup light
         const cmpLightDirectional = new ƒ.ComponentLight(new ƒ.LightDirectional(new ƒ.Color(0.5, 0.5, 0.5)));
-        cmpLightDirectional.mtxPivot.rotateY(180);
-        rotatorY.addComponent(cmpLightDirectional);
+        // cmpLightDirectional.mtxPivot.rotateY(180);
+        graph.addComponent(cmpLightDirectional);
         const cmpLightAmbient = new ƒ.ComponentLight(new ƒ.LightAmbient(new ƒ.Color(0.5, 0.5, 0.5)));
-        scene.addComponent(cmpLightAmbient);
-        // setup viewport
+        graph.addComponent(cmpLightAmbient);
         const viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", scene, camera.getComponent(ƒ.ComponentCamera), canvas);
-        viewport.draw();
-        console.log(viewport);
-        console.log(ƒ.Project.serialize());
-        // run loop
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, () => update(viewport, rotatorX.mtxLocal, rotatorY.mtxLocal, camera.mtxLocal));
+        const canvas = document.querySelector("canvas");
+        viewport.initialize("Viewport", graph, cmpCamera, canvas);
+        viewport.canvas.addEventListener("pointermove", hndPointerMove);
+        viewport.canvas.addEventListener("wheel", hndWheelMove);
+        canvas.addEventListener("mousedown", canvas.requestPointerLock);
+        canvas.addEventListener("mouseup", () => document.exitPointerLock());
+        let timeSpan = document.querySelector("span");
+        let gPressed = false;
+        let iShader = 0;
+        const shaders = [ƒ.ShaderFlatSkin, ƒ.ShaderGouraudSkin, ƒ.ShaderPhongSkin];
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start();
+        function update(_event) {
+            cmpLightDirectional.mtxPivot.rotation = new ƒ.Vector3(0, camera.rotationY + 180, 0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.P]))
+                ƒ.Time.game.setScale(0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W]))
+                ƒ.Time.game.setScale(0.1);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S]))
+                ƒ.Time.game.setScale(1);
+            const setShader = _shader => {
+                for (const node of graph) {
+                    if (node.getComponent(ƒ.ComponentMaterial))
+                        node.getComponent(ƒ.ComponentMaterial).material.setShader(_shader);
+                }
+            };
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.G])) {
+                if (!gPressed) {
+                    gPressed = true;
+                    setShader(shaders[iShader = (iShader + 1) % shaders.length]);
+                }
+            }
+            else
+                gPressed = false;
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.H]))
+                setShader(ƒ.ShaderPhong);
+            let cmpAnimator = graph.getComponent(ƒ.ComponentAnimator);
+            if (cmpAnimator)
+                timeSpan.innerText = cmpAnimator.time.toFixed(0);
+            viewport.draw();
+            viewport.draw();
+        }
     }
-    let gPressed = false;
-    let iShader = 0;
-    const shaders = [ƒ.ShaderFlatSkin, ƒ.ShaderGouraudSkin, ƒ.ShaderPhongSkin];
-    function update(_viewport, _mtxRotatorX, _mtxRotatorY, _mtxCamera) {
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.P]))
-            ƒ.Time.game.setScale(0);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W]))
-            ƒ.Time.game.setScale(0.1);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S]))
-            ƒ.Time.game.setScale(1);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
-            _mtxRotatorY.rotateY(3);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP]))
-            _mtxRotatorX.rotateX(-3);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT]))
-            _mtxRotatorY.rotateY(-3);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN]))
-            _mtxRotatorX.rotateX(3);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
-            _mtxRotatorX.set(ƒ.Matrix4x4.IDENTITY());
-            _mtxRotatorY.set(ƒ.Matrix4x4.IDENTITY());
-        }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.PAGE_UP])) {
-            // _mtxCamera.translateX(-mouse.position.x / _mtxCamera.translation.z);
-            // _mtxCamera.translateY(-mouse.position.y / _mtxCamera.translation.z);
-            _mtxCamera.translateZ(_mtxCamera.translation.z * 0.1);
-        }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.PAGE_DOWN])) {
-            // _mtxCamera.translateX(mouse.position.x / _mtxCamera.translation.z);
-            // _mtxCamera.translateY(mouse.position.y / _mtxCamera.translation.z);
-            _mtxCamera.translateZ(-_mtxCamera.translation.z * 0.1);
-        }
-        const setShader = _shader => {
-            for (const node of _viewport.getBranch()) {
-                if (node.getComponent(ƒ.ComponentMaterial))
-                    node.getComponent(ƒ.ComponentMaterial).material.setShader(_shader);
-            }
-        };
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.G])) {
-            if (!gPressed) {
-                gPressed = true;
-                setShader(shaders[iShader = (iShader + 1) % shaders.length]);
-            }
-        }
-        else
-            gPressed = false;
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.H]))
-            setShader(ƒ.ShaderPhong);
-        let cmpAnimator = _viewport.getBranch().getComponent(ƒ.ComponentAnimator);
-        if (cmpAnimator)
-            timeSpan.innerText = cmpAnimator.time.toFixed(0);
-        _viewport.draw();
+    function hndPointerMove(_event) {
+        if (!_event.buttons)
+            return;
+        cntMouseX.setInput(-_event.movementX);
+        cntMouseY.setInput(-_event.movementY);
+    }
+    function hndWheelMove(_event) {
+        camera.distance += _event.deltaY * speedCameraTranslation;
     }
 })(SkeletonTest || (SkeletonTest = {}));
 //# sourceMappingURL=FBXLoaderTest.js.map
--- Test/FBX/FBXLoaderTest.ts
+++ Test/FBX/FBXLoaderTest.ts
@@ -1,35 +1,37 @@
-///<reference path="./../../Core/Build/FudgeCore.d.ts"/>
+///<reference path="../../Core/Build/FudgeCore.d.ts"/>
+///<reference path="../../Aid/Build/FudgeAid.d.ts"/>
 namespace SkeletonTest {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
 
-  const mouse: {
-    position: ƒ.Vector2
-  } = {
-    position: new ƒ.Vector2()
-  };
+  let camera: ƒAid.CameraOrbit;
+  let speedCameraRotation: number = 0.2;
+  let speedCameraTranslation: number = 0.1;
+  let cntMouseX: ƒ.Control = new ƒ.Control("MouseX", speedCameraRotation);
+  let cntMouseY: ƒ.Control = new ƒ.Control("MouseY", speedCameraRotation);
 
   window.addEventListener("load", init);
-  let timeSpan: HTMLSpanElement;
-
   async function init(): Promise<void> {
-    const canvas: HTMLCanvasElement = document.querySelector("canvas") as HTMLCanvasElement;
-    timeSpan = document.querySelector("span") as HTMLElement;
+
     // const loader: ƒ.FBXLoader = await ƒ.FBXLoader.LOAD("./TriangularPrism.fbx");
     // const loader: ƒ.FBXLoader = await ƒ.FBXLoader.LOAD("./animated_arm.fbx");
     const loader: ƒ.FBXLoader = await ƒ.FBXLoader.LOAD("./Unarmed Walk Forward.fbx");
 
-    // track mouse position relative to canvas center
-    window.addEventListener("mousemove", (_event: MouseEvent) => {
-      mouse.position.x = _event.clientX - canvas.width / 2;
-      mouse.position.y = _event.clientY - canvas.height / 2;
-    });
-
     // test loading a mesh
-    console.log(await loader.getMesh(0));
+    // console.log(await loader.getMesh(0));
 
     // load scene
-    const scene: ƒ.Node = await loader.getScene(0);
-    console.log(scene);
+    const graph: ƒ.Node = await loader.getScene(0);
+    console.log(graph);
+
+    // camera setup
+    const cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    camera = new ƒAid.CameraOrbit(cmpCamera, 500, 80, 2, 1000);
+    camera.axisRotateX.addControl(cntMouseY);
+    camera.axisRotateY.addControl(cntMouseX);
+    cmpCamera.clrBackground.setHex("4472C4FF")
+    graph.addChild(camera);
+    camera.mtxLocal.translateY(100);
 
     // let skeleton: ƒ.Node = scene;
     // for (const node of scene)
@@ -66,88 +68,66 @@ namespace SkeletonTest {
     // console.log(loader.nodes);
     // console.log(loader.fbx);
 
-    // setup camera
-    const rotatorX: ƒ.Node = new ƒ.Node("RotatorX");
-    rotatorX.addComponent(new ƒ.ComponentTransform());
-    scene.addChild(rotatorX);
-
-    const rotatorY: ƒ.Node = new ƒ.Node("RotatorY");
-    rotatorY.addComponent(new ƒ.ComponentTransform());
-    rotatorX.addChild(rotatorY);
-
-    const camera: ƒ.Node = new ƒ.Node("Camera");
-    camera.addComponent(new ƒ.ComponentCamera());
-    camera.addComponent(new ƒ.ComponentTransform());
-    camera.getComponent(ƒ.ComponentCamera).clrBackground.setHex("4472C4FF");
-    rotatorX.mtxLocal.translateY(80);  // 80
-    camera.mtxLocal.translateZ(300); // 30
-    camera.mtxLocal.lookAt(ƒ.Vector3.Y(camera.mtxLocal.translation.y), camera.mtxLocal.getY());
-    rotatorY.addChild(camera);
-
     // setup light
     const cmpLightDirectional: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightDirectional(new ƒ.Color(0.5, 0.5, 0.5)));
-    cmpLightDirectional.mtxPivot.rotateY(180);
-    rotatorY.addComponent(cmpLightDirectional);
+    // cmpLightDirectional.mtxPivot.rotateY(180);
+    graph.addComponent(cmpLightDirectional);
 
     const cmpLightAmbient: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightAmbient(new ƒ.Color(0.5, 0.5, 0.5)));
-    scene.addComponent(cmpLightAmbient);
+    graph.addComponent(cmpLightAmbient);
 
-    // setup viewport
     const viewport: ƒ.Viewport = new ƒ.Viewport();
-    viewport.initialize("Viewport", scene, camera.getComponent(ƒ.ComponentCamera), canvas);
-    viewport.draw();
-    console.log(viewport);
-    
-    console.log(ƒ.Project.serialize());
+    const canvas: HTMLCanvasElement = document.querySelector("canvas") as HTMLCanvasElement;
+    viewport.initialize("Viewport", graph, cmpCamera, canvas);
+    viewport.canvas.addEventListener("pointermove", hndPointerMove);
+    viewport.canvas.addEventListener("wheel", hndWheelMove);
 
-    // run loop
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, () => 
-      update(viewport, rotatorX.mtxLocal, rotatorY.mtxLocal, camera.mtxLocal));
+    canvas.addEventListener("mousedown", canvas.requestPointerLock);
+    canvas.addEventListener("mouseup", () => document.exitPointerLock());
+
+    let timeSpan: HTMLSpanElement = document.querySelector("span") as HTMLElement;
+    let gPressed: boolean = false;
+    let iShader: number = 0;
+    const shaders: typeof ƒ.Shader[] = [ƒ.ShaderFlatSkin, ƒ.ShaderGouraudSkin, ƒ.ShaderPhongSkin];
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();
+
+    function update(_event: Event): void {
+      cmpLightDirectional.mtxPivot.rotation = new ƒ.Vector3(0, camera.rotationY + 180, 0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.P])) ƒ.Time.game.setScale(0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W])) ƒ.Time.game.setScale(0.1);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S])) ƒ.Time.game.setScale(1);
+      const setShader: (_shader: typeof ƒ.Shader) => void = _shader => {
+        for (const node of graph) {
+          if (node.getComponent(ƒ.ComponentMaterial))
+            node.getComponent(ƒ.ComponentMaterial).material.setShader(_shader);
+        }
+      };
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.G])) {
+        if (!gPressed) {
+          gPressed = true;
+          setShader(shaders[iShader = (iShader + 1) % shaders.length]);
+        }
+      } else 
+        gPressed = false;
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.H])) setShader(ƒ.ShaderPhong);
+      let cmpAnimator: ƒ.ComponentAnimator = graph.getComponent(ƒ.ComponentAnimator);
+      if (cmpAnimator)
+        timeSpan.innerText = cmpAnimator.time.toFixed(0);
+      viewport.draw();
+      viewport.draw();
+    }
   }
 
-  let gPressed: boolean = false;
-  let iShader: number = 0;
-  const shaders: typeof ƒ.Shader[] = [ƒ.ShaderFlatSkin, ƒ.ShaderGouraudSkin, ƒ.ShaderPhongSkin];
-  function update(_viewport: ƒ.Viewport, _mtxRotatorX: ƒ.Matrix4x4, _mtxRotatorY: ƒ.Matrix4x4, _mtxCamera: ƒ.Matrix4x4): void {
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.P])) ƒ.Time.game.setScale(0);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W])) ƒ.Time.game.setScale(0.1);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S])) ƒ.Time.game.setScale(1);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) _mtxRotatorY.rotateY(3);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP])) _mtxRotatorX.rotateX(-3);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) _mtxRotatorY.rotateY(-3);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN])) _mtxRotatorX.rotateX(3);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
-      _mtxRotatorX.set(ƒ.Matrix4x4.IDENTITY());
-      _mtxRotatorY.set(ƒ.Matrix4x4.IDENTITY());
-    }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.PAGE_UP])) {
-      // _mtxCamera.translateX(-mouse.position.x / _mtxCamera.translation.z);
-      // _mtxCamera.translateY(-mouse.position.y / _mtxCamera.translation.z);
-      _mtxCamera.translateZ(_mtxCamera.translation.z * 0.1);
-    }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.PAGE_DOWN])) {
-      // _mtxCamera.translateX(mouse.position.x / _mtxCamera.translation.z);
-      // _mtxCamera.translateY(mouse.position.y / _mtxCamera.translation.z);
-      _mtxCamera.translateZ(-_mtxCamera.translation.z * 0.1);
-    }
-    const setShader: (_shader: typeof ƒ.Shader) => void = _shader => {
-      for (const node of _viewport.getBranch()) {
-        if (node.getComponent(ƒ.ComponentMaterial))
-          node.getComponent(ƒ.ComponentMaterial).material.setShader(_shader);
-      }
-    };
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.G])) {
-      if (!gPressed) {
-        gPressed = true;
-        setShader(shaders[iShader = (iShader + 1) % shaders.length]);
-      }
-    }
-    else gPressed = false;
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.H])) setShader(ƒ.ShaderPhong);
-    let cmpAnimator: ƒ.ComponentAnimator = _viewport.getBranch().getComponent(ƒ.ComponentAnimator);
-    if (cmpAnimator)
-      timeSpan.innerText = cmpAnimator.time.toFixed(0);
-    _viewport.draw();
+  function hndPointerMove(_event: PointerEvent): void {
+    if (!_event.buttons)
+      return;
+    cntMouseX.setInput(-_event.movementX);
+    cntMouseY.setInput(-_event.movementY);
+  }
+
+  function hndWheelMove(_event: WheelEvent): void {
+    camera.distance += _event.deltaY * speedCameraTranslation;
   }
 }
